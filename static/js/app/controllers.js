f4e967f23868e39f9d8e21b01528d4fa9315550c
ramirezg <gabriel.ramirez@gsa.gov>
2015-07-22 10:35:52 -0400

    using dropdown for orgs on front-end

--- static/js/app/controllers.js
+++ static/js/app/controllers.js
@@ -1,63 +1,51 @@
 app.controller('HomeCtrl', function($scope, $http) {
+    'use strict';
     $scope.title = "Cf-Console";
     $http.get('/ping') // Just attempt to use the test route in the Go backend server.
         .success(function(response) {
-            $scope.backendStatus = response
+            $scope.backendStatus = response;
         }).error(function(response) {
-            $scope.backendStatus = "offline"
+            $scope.backendStatus = "offline";
         });
     // TODO. Check if logged in. If so, redirect to the "dashboard".
     // Otherwise, reload the login page.
 });
 
 
-app.controller('DashboardCtrl', function($scope, $http, $location) {
-
-    load_data = function(endpoint, scope_name) {
-       $http.get(endpoint).success(function(response) {
-            $scope[scope_name] = response.resources
+app.controller('DashboardCtrl', function($scope, $http) {
+    'use strict';
+    $http.get('/v2/organizations').success(function(response) {
+        $scope.orgs = response.resources;
+    });
+    $scope.showOrg = function(org) {
+        $scope.activeOrg = org;
+        $scope.orgVisible = true;
+        $http.get(org.entity.spaces_url).success(function(response) {
+            var resources = response.resources;
+            if (resources.length > 0) {
+                $scope.activeSpaces = response.resources;
+                $scope.noSpaces = false;
+            } else {
+                $scope.noSpaces = true;
+                $scope.activeSpaces = false;
+            }
         });
-    }
-    load_data('/v2/quota_definitions', 'quotas');
-    load_data('/v2/organizations', 'orgs');
-
-});
-
-app.controller('OrgController', function($scope, $http) {
-    // Get the quota for the org
-    quota = $scope.quotas.filter(function (element){
-         return element.metadata.guid == $scope.org.entity.quota_definition_guid;
-     });
-    if (quota.length > 0)
-        $scope.quota = quota[0];
-
-    // Get the spaces and open the div
-    $scope.getSpaces = function(){
-        if($scope.openOrgSpaces) {
-            $scope.openOrgSpaces = false;
-        }
-        else{
-            $http.get($scope.org.entity.spaces_url).success(function(response) {
-                $scope.spaces = response.resources;
-                $scope.openOrgSpaces = true;
-            });
-        }
-
-    }
+    };
+    $scope.clearDashboard = function() {
+        $scope.orgVisible = false;
+        $scope.activeOrg = false;
+    };
 });
 
 app.controller('SpaceController', function($scope, $http) {
+    'use strict';
     // Get the apps for spaces
-    $scope.getApps = function(){
-       $http.get($scope.space.entity.apps_url).success(function(response) {
-            var resources = response.resources;
-            if(resources.length > 0)
-                $scope.apps = response.resources;
-        });
-    }
-});
-
-app.controller('AppController', function($scope, $http) {
-    // For functions like reset
-
+    $http.get($scope.space.entity.apps_url).success(function(response) {
+        var resources = response.resources;
+        if (resources.length > 0) {
+            $scope.apps = response.resources;
+        } else {
+            $scope.noApps = true;
+        }
+    });
 });
