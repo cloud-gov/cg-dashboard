90874d501f7549036f9406f7e8461c56774b611e
James C. Scott <jamesc.scott@gsa.gov>
2015-07-27 12:51:40 -0400

    Initial app structure test.

---
+++ acceptance/app_structure_test.go
@@ -0,0 +1,24 @@
+package acceptance_test
+
+import (
+	. "github.com/18F/cf-console/acceptance"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/sclevine/agouti"
+	. "github.com/sclevine/agouti/matchers"
+)
+
+var _ = Describe("AppStructure", func() {
+	var page *agouti.Page
+
+	BeforeEach(func() {
+		var err error
+		page, err = agoutiDriver.NewPage()
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		Expect(page.Destroy()).To(Succeed())
+	})
+})
