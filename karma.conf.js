c04ebba535e5283618f761a7f43715950c243bb4
ramirezg <gabriel.ramirez@gsa.gov>
2015-07-29 10:11:21 -0400

    adding codecov support

--- karma.conf.js
+++ karma.conf.js
@@ -64,6 +64,13 @@ module.exports = function(config) {
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false
+        singleRun: false,
+
+        coverageReporter: {
+            type: 'json',
+            dir: 'coverage',
+            subdir: '.'
+        }
+
     })
 }
