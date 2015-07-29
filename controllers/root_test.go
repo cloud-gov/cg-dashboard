4a2e0d765cb8433ae082c40b1262115ecab37b3f
James C. Scott <jamesc.scott@gsa.gov>
2015-07-29 14:29:38 -0400

    Added coverage for LoginHandshake

--- controllers/root_test.go
+++ controllers/root_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/18F/cf-console/helpers"
 	"github.com/18F/cf-console/helpers/testhelpers"
 
+	"strings"
 	"testing"
 )
 
@@ -18,15 +19,16 @@ func TestPing(t *testing.T) {
 }
 
 type loginHandshakeTest struct {
-	testName    string
-	envVars     helpers.EnvVars
-	sessionData map[string]interface{}
-	returnValue string
+	testName       string
+	envVars        helpers.EnvVars
+	sessionData    map[string]interface{}
+	returnCode     int
+	returnLocation string
 }
 
 var loginHandshakeTests = []loginHandshakeTest{
 	{
-		testName: "Login Handshake Tests",
+		testName: "Login Handshake With Already Authenticated User",
 		envVars: helpers.EnvVars{
 			ClientID:     "ID",
 			ClientSecret: "Secret",
@@ -35,10 +37,38 @@ var loginHandshakeTests = []loginHandshakeTest{
 			UAAURL:       "uaaurl",
 			APIURL:       "apiurl",
 		},
-		sessionData: testhelpers.ValidTokenData,
-		returnValue: "{\"status\": \"authorized\"}",
+		sessionData:    testhelpers.ValidTokenData,
+		returnCode:     302,
+		returnLocation: "/#/dashboard",
+	},
+	{
+		testName: "Login Handshake With Already Authenticated User",
+		envVars: helpers.EnvVars{
+			ClientID:     "ID",
+			ClientSecret: "Secret",
+			Hostname:     "hostname",
+			LoginURL:     "loginurl",
+			UAAURL:       "uaaurl",
+			APIURL:       "apiurl",
+		},
+		returnCode:     302,
+		returnLocation: "/oauth/authorize",
 	},
 }
 
 func TestLoginHandshake(t *testing.T) {
+	response, request := testhelpers.NewTestRequest("GET", "/handshake")
+	for _, test := range loginHandshakeTests {
+		router := testhelpers.CreateRouterWithMockSession(test.sessionData, test.envVars)
+		router.ServeHTTP(response, request)
+		// Check the return code.
+		if response.Code != test.returnCode {
+			t.Errorf("Expected http code %d, Found http code %d\n", test.returnCode, response.Code)
+		}
+		// Check the location of where we relocated to.
+		if !strings.Contains(response.Header().Get("Location"), test.returnLocation) {
+			t.Errorf("Expected http location %d, Found http code %d\n", test.returnLocation, response.Header().Get("Location"))
+		}
+	}
+
 }
