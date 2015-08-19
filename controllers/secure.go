99b165d530a35349af3aa4fd6a264a072f141acb
James C. Scott <jamesc.scott@gsa.gov>
2015-08-19 18:33:18 -0400

    Remove redundant code

--- controllers/secure.go
+++ controllers/secure.go
@@ -35,6 +35,28 @@ func (c *SecureContext) OAuth(rw web.ResponseWriter, req *web.Request, next web.
 // Proxy is an internal function that will construct the client with the token in the headers and
 // then send a request.
 func (c *SecureContext) Proxy(rw http.ResponseWriter, req *http.Request, url string) {
+	// Acquire the http client and the refresh token if needed
+	// https://godoc.org/golang.org/x/oauth2#Config.Client
+	client := c.Settings.OAuthConfig.Client(c.Settings.TokenContext, &c.Token)
+	c.submitRequest(rw, req, url, client)
+}
+
+// PrivilegedProxy is an internal function that will construct the client using
+// the credentials of the web app itself (not of the user) with the token in the headers and
+// then sends a request.
+func (c *SecureContext) PrivilegedProxy(rw http.ResponseWriter, req *http.Request, url string) {
+	// Acquire the http client and the refresh token if needed
+	// https://godoc.org/golang.org/x/oauth2#Config.Client
+	client := c.Settings.HighPrivilegedOauthConfig.Client(c.Settings.TokenContext)
+	c.submitRequest(rw, req, url, client)
+
+}
+
+// submitRequest uses a given client and submits the specified request.
+func (c *SecureContext) submitRequest(rw http.ResponseWriter, req *http.Request, url string, client *http.Client) {
+	// Prevents lingering goroutines from living forever.
+	// http://stackoverflow.com/questions/16895294/how-to-set-timeout-for-http-get-requests-in-golang/25344458#25344458
+	client.Timeout = 5 * time.Second
 	// In case the body is not of io.Closer.
 	if req.Body != nil {
 		defer req.Body.Close()
@@ -43,12 +65,6 @@ func (c *SecureContext) Proxy(rw http.ResponseWriter, req *http.Request, url str
 	// Make a new request.
 	request, _ := http.NewRequest(req.Method, url, req.Body)
 	request.Close = true
-	// Acquire the http client and the refresh token if needed
-	// https://godoc.org/golang.org/x/oauth2#Config.Client
-	client := c.Settings.OAuthConfig.Client(c.Settings.TokenContext, &c.Token)
-	// Prevents lingering goroutines from living forever.
-	// http://stackoverflow.com/questions/16895294/how-to-set-timeout-for-http-get-requests-in-golang/25344458#25344458
-	client.Timeout = 5 * time.Second
 	// Send the request.
 	res, err := client.Do(request)
 	if res != nil {
@@ -68,32 +84,6 @@ func (c *SecureContext) Proxy(rw http.ResponseWriter, req *http.Request, url str
 		fmt.Fprintf(rw, "unknown error. try again")
 		return
 	}
-	// Write the body into response that is going back to the frontend.
-	fmt.Fprintf(rw, string(body))
-}
-
-// PrivilegedProxy is an internal function that will construct the client using
-// the credentials of the web app itself (not of the user) with the token in the headers and
-// then sends a request.
-func (c *SecureContext) PrivilegedProxy(rw http.ResponseWriter, req *http.Request, url string) {
-	// Acquire the http client and the refresh token if needed
-	// https://godoc.org/golang.org/x/oauth2#Config.Client
-	client := c.Settings.HighPrivilegedOauthConfig.Client(c.Settings.TokenContext)
-	c.submitRequest(rw, req, url, client)
-
-}
-
-// submitRequest uses a given client and submits the specified request.
-func (c *SecureContext) submitRequest(rw http.ResponseWriter, req *http.Request, url string, client *http.Client) {
-	// Make the request.
-	request, _ := http.NewRequest(req.Method, url, req.Body)
-	// Send the request.
-	res, _ := client.Do(request)
-	// Should return the same status.
-	rw.WriteHeader(res.StatusCode)
-	// Read the body.
-	body, _ := ioutil.ReadAll(res.Body)
-	defer res.Body.Close()
 	// Write the body into response that is going back to the frontend.
 	fmt.Fprintf(rw, string(body))
 }
