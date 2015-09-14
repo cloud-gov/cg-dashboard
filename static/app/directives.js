170964de2fe79555b3c02c89e8428b0af3ad4d55
ramirezg <gabriel.ramirez@gsa.gov>
2015-09-14 15:26:40 -0400

    stopping users from going to #/dashboard without being authed

--- static/app/directives.js
+++ static/app/directives.js
@@ -27,71 +27,79 @@
                             } else {
                                 $scope.view.status = "error";
                             }
-                             $scope.view.disableButton = false;
+                            $scope.view.disableButton = false;
                         });
                 };
             }
         };
     });
-    app.directive('deckHeader', function($cloudfoundry, $uaa) {
+    app.directive('deckHeader', function($cloudfoundry, $uaa, MenuData) {
         return {
             templateUrl: 'app/views/partials/header.html',
             controller: function($scope, $location) {
-              // Render the orgs on the page
-              var renderOrgs = function(orgs) {
-                  $scope.orgs = orgs;
-                  $cloudfoundry.setOrgsData(orgs);
-              };
-              var renderName = function(name) {
-                  $scope.givenName = name;
-                  // Load user's permissions
-                  $uaa.getUsersPermissions('managed_organizations')
-                  // Only allow navigation when names are loaded
-                  $scope.authorized = true;
-              };
-              // Get the auth status
-              $cloudfoundry.getAuthStatus().then(function (status) {
-                if (status === "authorized") {
-                  // Load the org data
-                  $cloudfoundry.getOrgsData(renderOrgs);
-                  // Load username
-                  $uaa.getUserInfoGivenName().then(renderName);
-                }
-                else {
-                  $scope.authorized = false;
-                  $location.path('/');
-                }
-              });
+                // Initalize menudata
+                $scope.MenuData = MenuData;
+
+                // Render the orgs on the page
+                var renderOrgs = function(orgs) {
+                    $scope.orgs = orgs;
+                    $cloudfoundry.setOrgsData(orgs);
+                };
+                var renderName = function(name) {
+                    $scope.givenName = name;
+                    // Load user's permissions
+                    $uaa.getUsersPermissions('managed_organizations')
+                        // Only allow navigation when names are loaded
+                    $scope.authorized = true;
+                };
+                // Get the auth status
+                $cloudfoundry.getAuthStatus().then(function(status) {
+                    if (status === "authorized") {
+                        // Load the org data
+                        $cloudfoundry.getOrgsData(renderOrgs);
+                        // Load username
+                        $uaa.getUserInfoGivenName().then(renderName);
+                    } else {
+                        $scope.authorized = false;
+                        $location.path('/');
+                    }
+                });
             }
         };
     });
     app.directive('cloudStatus', function($window) {
-      return {
-          templateUrl: 'app/views/partials/status.html',
-          controller: function($scope, $http, $window, $interval) {
-              var sp = new StatusPage.page({ page : 'swcbylb1c30f' });
-              var loadStatus = function(sp) {
-                sp.status({
-                  success: function(data) {
-                    $scope.$apply(function(){
-                      $scope.statuspagecolor = "statuspagecolor-light " + data.status.indicator;
-                    });
-                  }
-                })
-              };
-              // Set up the poll to call loadStatus(sp) every 10 seconds.
-              var poll = $interval(function(){loadStatus(sp);}, 10000);
-              // Call loadStatus(sp) for the first time.
-              loadStatus(sp);
-              // Destroy the poll when the page is left to clean up.
-              $scope.$on('$destroy', function() {$interval.cancel(poll);});
+        return {
+            templateUrl: 'app/views/partials/status.html',
+            controller: function($scope, $http, $window, $interval) {
+                var sp = new StatusPage.page({
+                    page: 'swcbylb1c30f'
+                });
+                var loadStatus = function(sp) {
+                    sp.status({
+                        success: function(data) {
+                            $scope.$apply(function() {
+                                $scope.statuspagecolor = "statuspagecolor-light " + data.status.indicator;
+                            });
+                        }
+                    })
+                };
+                // Set up the poll to call loadStatus(sp) every 10 seconds.
+                var poll = $interval(function() {
+                    loadStatus(sp);
+                }, 10000);
+                // Call loadStatus(sp) for the first time.
+                loadStatus(sp);
+                // Destroy the poll when the page is left to clean up.
+                $scope.$on('$destroy', function() {
+                    $interval.cancel(poll);
+                });
 
-          },
-          link: function($scope) {
-            $scope.goto = function() {
-                $window.open('https://cloudgov.statuspage.io/');
-            };
-          }
-      };
+            },
+            link: function($scope) {
+                $scope.goto = function() {
+                    $window.open('https://cloudgov.statuspage.io/');
+                };
+            }
+        };
     });
 }());
