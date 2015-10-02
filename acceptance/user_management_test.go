d51bc3f284a312984fe84dc8d48122d5c1023028
ramirezg <gabriel.ramirez@gsa.gov>
2015-10-02 12:00:02 -0400

    updating user managment space tests

---
+++ acceptance/user_management_test.go
@@ -0,0 +1,64 @@
+// +build acceptance
+
+package acceptance
+
+import (
+	. "github.com/18F/cf-deck/acceptance/util"
+	. "github.com/18F/cf-deck/acceptance/views"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/sclevine/agouti"
+	"net/http/httptest"
+)
+
+var _ = Describe("AppStructure", func() {
+	var (
+		user        User
+		page        *agouti.Page
+		server      *httptest.Server
+		testEnvVars AcceptanceTestEnvVars
+		spaces      Spaces
+		space       Space
+	)
+
+	testEnvVars = AcceptanceTestEnvVars{}
+	testEnvVars.LoadTestEnvVars()
+
+	BeforeEach(func() {
+		// Start a test server
+		server, testEnvVars = startServer()
+
+		// Create a fresh page to navigate.
+		page = createPage()
+
+		// Create user
+		user = StartUserSessionWith(testEnvVars)
+
+		// Log user in
+		user.LoginTo(page)
+	})
+
+	It("Allow the user to modify user permissions for space", func() {
+		By("going to the space user management page", func() {
+			user.OpenDropdownOfOrgsOn(page)
+			user.SelectOrgFromDropdown(page, testEnvVars.TestOrgName)
+			spaces = user.OpenOrgMenuOn(page).ClickSpacesLink()
+			space = spaces.ViewSpace(testEnvVars.TestSpaceName)
+			space.ViewsUserManagementTab()
+		})
+
+		By("allowing the user to add and remove a space user", func() {
+			space.ToggleUser(testEnvVars.TestOrgName, testEnvVars.Username)
+		})
+
+	})
+
+	AfterEach(func() {
+		// Logout user
+		user.LogoutOf(page)
+		// Destroy the page
+		Expect(page.Destroy()).To(Succeed())
+		// Close the server.
+		server.Close()
+	})
+})
--- acceptance/views/space.go
+++ acceptance/views/space.go
@@ -1,6 +1,7 @@
 // +build acceptance
 
 package util
+
 import (
 	. "github.com/18F/cf-deck/acceptance/util"
 	. "github.com/onsi/gomega"
@@ -26,4 +27,55 @@ func (s Space) ViewServiceInstances() Services {
 	Eventually(s.page.FindByLink("Service Instances")).Should(BeFound())
 	Expect(s.page.FindByLink("Service Instances").Click()).To(Succeed())
 	return Services{s.page}
+}
+
+func (s Space) ViewsUserManagementTab() {
+	DelayForRendering()
+	Eventually(s.page.FindByLink("User Management")).Should(BeFound())
+	Expect(s.page.FindByLink("User Management").Click()).To(Succeed())
+}
+
+func (s Space) getToUserPerms(TestOrgName string, Username string) {
+	DelayForRendering()
+	Eventually(s.page.FindByLink("All " + TestOrgName + " Org Users")).Should(BeFound())
+	DelayForRendering()
+	Expect(s.page.FindByLink("All " + TestOrgName + " Org Users").Click()).To(Succeed())
+	DelayForRendering()
+	DelayForRendering()
+	Expect(s.page.FindByLink(Username).Click()).To(Succeed())
+}
+
+func (s Space) changeUserPermissions() string {
+	devToggle := s.page.Find("#developers")
+	Expect(devToggle.Click()).To(Succeed())
+	DelayForRendering()
+	newState, _ := devToggle.Find(".toggle-switch-animate").Attribute("class")
+	return newState
+}
+
+func (s Space) ToggleUser(TestOrgName string, Username string) {
+	s.getToUserPerms(TestOrgName, Username)
+	DelayForRendering()
+	// Get the original state
+	originalState, _ := s.page.Find("#developers").Find(".toggle-switch-animate").Attribute("class")
+	// Toggle the first time
+	firstStateChange := s.changeUserPermissions()
+	DelayForRendering()
+	// Check that the toggle changed
+	s.page.Refresh()
+	DelayForRendering()
+	s.getToUserPerms(TestOrgName, Username)
+	DelayForRendering()
+	currentState, _ := s.page.Find("#developers").Find(".toggle-switch-animate").Attribute("class")
+	Expect(firstStateChange).To(Equal(currentState))
+	// Toggle the second time
+	s.changeUserPermissions()
+	DelayForRendering()
+	// Check that the state returned to the original
+	s.page.Refresh()
+	DelayForRendering()
+	s.getToUserPerms(TestOrgName, Username)
+	DelayForRendering()
+	secondStateChange, _ := s.page.Find("#developers").Find(".toggle-switch-animate").Attribute("class")
+	Expect(originalState).To(Equal(secondStateChange))
 }
