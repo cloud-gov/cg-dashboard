04e57b59b265a7b233931404cd5fbc181fedbe69
Marco Segreto <marco.segreto@gsa.gov>
2015-10-08 14:27:56 -0700

    Get dropdown fully working
    Add code to dropdown to open and close it.
    
    Question: should this be handled by bootstraps code? I haven't imported
    the JS for it because I think it's too much code and it includes jQuery
    which I don't want.

--- static_src/components/dropdown.jsx
+++ static_src/components/dropdown.jsx
@@ -1,6 +1,8 @@
 
 import React from 'react';
 
+import classNames from 'classnames';
+
 export default class Dropdown extends React.Component {
   constructor(props) {
     super(props);
@@ -8,15 +10,27 @@ export default class Dropdown extends React.Component {
     this.state = { open: false };
   }
 
+  handleTitleClick = (ev) => {
+    this.setState({ open: !this.state.open });
+  }
+
   render() {
+    var classes = classNames('dropdown', {
+      'open': !!this.state.open
+    }),
+        id = 'dropdown-' + this.props.title;
+
     return (
-      <div className="dropdown open">
-        <a className="dropdown-toggle" role="button"
-            aria-haspopup="true" aria-expanded="{ this.state.open }">
+      <div className={ classes }>
+      <a id="dropdown-{ this.props.title }" 
+            className="dropdown-toggle" role="button"
+            aria-haspopup="true" aria-expanded={ id }
+            onClick={ this.handleTitleClick }>
           { this.props.title }
           <span className="caret"></span>
         </a>
-        <ul className="dropdown-menu">
+        <ul className="dropdown-menu" 
+            aria-labelledby={ id }>
           { this.props.items.map((item) => {
             return (
               <li key={ item.key }>{ item.element }</li>
