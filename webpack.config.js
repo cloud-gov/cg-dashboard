f692ba2d5415f67172154b596d2c3d57f71af117
Marco Segreto <marco.segreto@gsa.gov>
2016-03-30 15:48:08 -0700

    Start at adding uswds

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+
 var path = require('path');
 
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -19,7 +20,7 @@ module.exports = {
 
   module: {
     loaders: [
-      { 
+      {
         test: /\.jsx?$/,
         loader: 'babel',
         exclude: [
@@ -30,24 +31,36 @@ module.exports = {
           plugins: ['transform-runtime']
         }
       },
-      { test: /\.css$/, 
+      { test: /\.css$/,
         include: path.resolve(__dirname, 'static_src/css'),
         loader: ExtractTextPlugin.extract('style-loader',
-          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') 
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
       },
       { test: /\.less$/,
         loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+      },
+      { test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
       },
       { test: /\.css$/,
         include: path.resolve(__dirname, 'node_modules'),
         loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
       },
-      { test: /\.(woff|woff2)$/,  
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+      { test: /\.(woff|woff2)$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
       },
-      { test: /\.ttf$/,    loader: 'file-loader' },
-      { test: /\.eot$/,    loader: 'file-loader' },
-      { test: /\.svg$/,    loader: 'file-loader' }
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      },
+      { test: /\.png$/,  loader: "url?limit=10000&mimetype=image/png" },
     ]
   },
 
