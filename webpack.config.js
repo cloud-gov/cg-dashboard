caffd01d593ac09afd03b130de7dfe427d32960b
Marco Segreto <marco.segreto@gsa.gov>
2016-03-30 15:48:08 -0700

    Start at adding uswds

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+
 var path = require('path');
 
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -38,16 +39,16 @@ module.exports = {
       { test: /\.less$/,
         loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
       },
+      { test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+      },
       { test: /\.css$/,
         include: path.resolve(__dirname, 'node_modules'),
         loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
       },
       { test: /\.(woff|woff2)$/,
         loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      { test: /\.ttf$/,    loader: 'file-loader' },
-      { test: /\.eot$/,    loader: 'file-loader' },
-      { test: /\.svg$/,    loader: 'file-loader' }
+      }
     ]
   },
 
