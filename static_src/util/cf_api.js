9432c8535c65beddd2ea59bba055c8039489f41c
Marco Segreto <marco.segreto@gsa.gov>
2016-04-06 16:59:27 -0700

    Linting fixes for app and util/cf_api
    Other changes:
    - Linting is now being done in main build rather then test.

--- static_src/util/cf_api.js
+++ static_src/util/cf_api.js
@@ -5,7 +5,6 @@ import 'promises-done-polyfill';
 import appActions from '../actions/app_actions.js';
 import errorActions from '../actions/error_actions.js';
 import loginActions from '../actions/login_actions.js';
-import loginActionTypes from '../constants.js';
 import orgActions from '../actions/org_actions.js';
 import spaceActions from '../actions/space_actions.js';
 import serviceActions from '../actions/service_actions.js';
@@ -19,7 +18,7 @@ export default {
   fetch(url, action, multiple, ...params) {
     return http.get(APIV + url).done((res) => {
       if (!multiple) {
-        action(res.data, ...params)
+        action(res.data, ...params);
       } else {
         action(res.data.resources, ...params);
       }
@@ -37,62 +36,56 @@ export default {
   },
 
   getAuthStatus() {
-    return http.get(APIV + '/authstatus').done((res) => {
+    return http.get(`${APIV}/authstatus`).done((res) => {
       loginActions.receivedStatus(res.data.status);
-    }, (err) => {
+    }, () => {
       loginActions.receivedStatus(false);
     });
   },
 
   fetchOrgLinks(guid) {
-    return http.get(APIV + '/organizations/' + guid).then((res) => {
-      return res.data.entity;
-    });
+    return http.get(`${APIV}/organizations/${guid}`).then((res) =>
+      res.data.entity);
   },
 
   fetchOrgDetails(guid) {
-    return http.get(APIV + '/organizations/' + guid + '/summary')
-        .then((res) => {
-      return res.data;
-    });
+    return http.get(`${APIV}/organizations/${guid}/summary`)
+        .then((res) => res.data);
   },
 
   fetchOrg(guid) {
-    var fullOrg = {};
-    var req = Promise.all([this.fetchOrgLinks(guid), this.fetchOrgDetails(guid)]);
+    let fullOrg = {};
+    const req = Promise.all(
+      [this.fetchOrgLinks(guid), this.fetchOrgDetails(guid)]);
 
     return req.then((res) => {
       fullOrg = Object.assign(...res);
       return Promise.all([
         this.fetchOrgMemoryUsage(guid),
         this.fetchOrgMemoryLimit(fullOrg)
-      ]); 
+      ]);
     }).then((res) => {
-      var quota = {}
+      let quota = {};
       quota = Object.assign(quota, ...res);
       fullOrg.quota = quota;
       orgActions.receivedOrg(fullOrg);
     }, (err) => {
-      errorActions.errorFetch(err); 
+      errorActions.errorFetch(err);
     });
   },
 
   fetchOrgMemoryUsage(guid) {
-    return http.get(APIV + '/organizations/' + guid + '/memory_usage')
-    .then((res) => {
-      return res.data; 
-    });;
+    return http.get(`${APIV}/organizations/${guid}/memory_usage`)
+      .then((res) => res.data);
   },
 
   fetchOrgMemoryLimit(org) {
     return http.get(org.quota_definition_url)
-    .then((res) => {
-      return res.data.entity; 
-    });;
+      .then((res) => res.data.entity);
   },
 
   fetchOrgs() {
-    return http.get(APIV + '/organizations').done((res) => {
+    return http.get(`${APIV}/organizations`).done((res) => {
       orgActions.receivedOrgs(res.data.resources);
     }, (err) => {
       errorActions.errorFetch(err);
@@ -100,41 +93,41 @@ export default {
   },
 
   fetchSpace(spaceGuid) {
-    return this.fetchOne(`/spaces/${ spaceGuid }/summary`,
+    return this.fetchOne(`/spaces/${spaceGuid}/summary`,
                          spaceActions.receivedSpace);
   },
 
   fetchServiceInstances(spaceGuid) {
-    return this.fetchMany(`/spaces/${ spaceGuid }/service_instances`,
+    return this.fetchMany(`/spaces/${spaceGuid}/service_instances`,
                           serviceActions.receivedInstances);
   },
 
   createServiceInstance(name, spaceGuid, servicePlanGuid) {
-    var payload = {
-      name: name,
+    const payload = {
+      name,
       space_guid: spaceGuid,
       service_plan_guid: servicePlanGuid
     };
 
-    return http.post(APIV + '/service_instances?accepts_incomplete=true', payload)
-    .done((res) => {
-      serviceActions.createdInstance(res.data);
-    }, (err) => {
-      serviceActions.errorCreateInstance(err.data);
-    });
+    return http.post(`${APIV}/service_instances?accepts_incomplete=true`, payload)
+      .done((res) => {
+        serviceActions.createdInstance(res.data);
+      }, (err) => {
+        serviceActions.errorCreateInstance(err.data);
+      });
   },
 
   deleteUnboundServiceInstance(serviceInstance) {
     return http.delete(serviceInstance.url)
-    .done((res) => {
+    .done(() => {
       serviceActions.deletedInstance(serviceInstance.guid);
-    }, (err) => {
+    }, () => {
       // Do nothing.
     });
   },
 
   fetchApp(appGuid) {
-    return this.fetchOne(`/apps/${ appGuid }/summary`,
+    return this.fetchOne(`/apps/${appGuid}/summary`,
                           appActions.receivedApp);
   },
 
@@ -144,8 +137,8 @@ export default {
    * @param {Number} spaceGuid - The guid of the space that the users belong to.
    */
   fetchSpaceUsers(spaceGuid) {
-    return this.fetchMany(`/spaces/${ spaceGuid }/user_roles`,
-                          userActions.receivedSpaceUsers, 
+    return this.fetchMany(`/spaces/${spaceGuid}/user_roles`,
+                          userActions.receivedSpaceUsers,
                           spaceGuid);
   },
 
@@ -155,79 +148,73 @@ export default {
    * @param {Number} orgGuid - The guid of the org that the users belong to.
    */
   fetchOrgUsers(orgGuid) {
-    return this.fetchMany(`/organizations/${ orgGuid }/users`,
+    return this.fetchMany(`/organizations/${orgGuid}/users`,
                           userActions.receivedOrgUsers,
                           orgGuid);
   },
 
   fetchOrgUserRoles(orgGuid) {
-    return this.fetchMany(`/organizations/${ orgGuid }/user_roles`,
+    return this.fetchMany(`/organizations/${orgGuid}/user_roles`,
                           userActions.receivedOrgUserRoles,
                           orgGuid);
   },
 
   deleteUser(userGuid, orgGuid) {
-    return http.delete(APIV + '/organizations/' + orgGuid + '/users/' + userGuid)
-        .done((res) => {
-      userActions.deletedUser(userGuid, orgGuid);
-    }, (err) => {
-      userActions.errorRemoveUser(userGuid, err.data);
-    });
-  },
-
-  // TODO deprecate possibly in favor of deleteOrgUserPermissions.
-  deleteOrgUserCategory(userGuid, orgGuid, category) {
-    return http.delete(APIV + '/organizations/' + orgGuid + '/' + category +
-                       '/' + userGuid).catch((err) => {
-      // TODO create correct error action.
-    });
-  },
-
-  deleteOrgUserPermissions(userGuid, orgGuid, permissions) {
-    return http.delete(APIV + '/organizations/' + orgGuid + '/' + permissions + 
-                       '/' + userGuid).then((res) => {
-      return res.response; 
-    }, (err) => {
-      userActions.errorRemoveUser(userGuid, err.data);
-    });
-  },
-
-  putOrgUserPermissions(userGuid, orgGuid, permissions) {
-    return http.put(APIV + '/organizations/' + orgGuid + '/' + permissions + 
-                       '/' + userGuid).then((res) => {
-      return res.response; 
-    });
-  },
-
-  // TODO refactor with org user permissions
-  putSpaceUserPermissions(userGuid, spaceGuid, role) {
-    return http.put(APIV + `/spaces/${ spaceGuid }/${ role }/${ userGuid }`).then(
-      (res) => {
-        return res.response;
-      }, (err) => {
-        // TODO figure out error action
-        console.error(err);
-      });
-  },
-
-  // TODO refactor with org user permissions
-  deleteSpaceUserPermissions(userGuid, spaceGuid, role) {
-    return http.delete(APIV + `/spaces/${ spaceGuid }/${ role }/${ userGuid }`).then(
-      (res) => {
-        return res.response;
+    return http.delete(`${APIV}/organizations/${orgGuid}/users/${userGuid}`)
+      .done(() => {
+        userActions.deletedUser(userGuid, orgGuid);
       }, (err) => {
         userActions.errorRemoveUser(userGuid, err.data);
       });
   },
 
+  // TODO deprecate possibly in favor of deleteOrgUserPermissions.
+  deleteOrgUserCategory(userGuid, orgGuid, category) {
+    return http.delete(`${APIV}/organizations/${orgGuid}/${category}
+      /${userGuid}`).catch(() => {
+        // TODO create correct error action.
+      });
+  },
+
+  deleteOrgUserPermissions(userGuid, orgGuid, permissions) {
+    return http.delete(`${APIV}/organizations/${orgGuid}/${permissions}
+       /${userGuid}`).then((res) =>
+        res.response
+      , (err) => {
+        userActions.errorRemoveUser(userGuid, err.data);
+      });
+  },
+
+  putOrgUserPermissions(userGuid, orgGuid, permissions) {
+    return http.put(`${APIV}/organizations/${orgGuid}/${permissions}
+        /${userGuid}`).then((res) => res.response
+    );
+  },
+
+  // TODO refactor with org user permissions
+  putSpaceUserPermissions(userGuid, spaceGuid, role) {
+    return http.put(`${APIV}/spaces/${spaceGuid}/${role}/${userGuid}`)
+      .then((res) => res.response, () => {
+        // TODO figure out error action
+      });
+  },
+
+  // TODO refactor with org user permissions
+  deleteSpaceUserPermissions(userGuid, spaceGuid, role) {
+    return http.delete(`${APIV}/spaces/${spaceGuid}/${role}/${userGuid}`)
+      .then((res) => res.response, (err) => {
+        userActions.errorRemoveUser(userGuid, err.data);
+      });
+  },
+
   fetchAllServices(orgGuid) {
-    return this.fetchMany('/organizations/' + orgGuid + '/services',
-                          serviceActions.receivedServices);
+    return this.fetchMany(`/organizations/${orgGuid}/services`,
+      serviceActions.receivedServices);
   },
 
   fetchAllServicePlans(serviceGuid) {
-    return this.fetchMany('/services/' + serviceGuid + '/service_plans',
-                          serviceActions.receivedPlans);
+    return this.fetchMany(`/services/${serviceGuid}/service_plans`,
+      serviceActions.receivedPlans);
   }
 
 };
