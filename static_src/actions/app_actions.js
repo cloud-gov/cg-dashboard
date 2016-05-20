39277be519ec83542b749d8e8dadc08360fd3e30
Marco <msegreto@miceover.com>
2016-05-20 15:22:37 -0700

    Fix lint errors for newly touched files

--- static_src/actions/app_actions.js
+++ static_src/actions/app_actions.js
@@ -11,29 +11,29 @@ export default {
   fetch(appGuid) {
     AppDispatcher.handleViewAction({
       type: appActionTypes.APP_FETCH,
-      appGuid: appGuid
+      appGuid
     });
   },
 
   receivedApp(app) {
     AppDispatcher.handleServerAction({
       type: appActionTypes.APP_RECEIVED,
-      app: app
+      app
     });
   },
 
   fetchStats(appGuid) {
     AppDispatcher.handleViewAction({
       type: appActionTypes.APP_STATS_FETCH,
-      appGuid: appGuid
+      appGuid
     });
   },
 
   receivedAppStats(appGuid, app) {
     AppDispatcher.handleServerAction({
       type: appActionTypes.APP_STATS_RECEIVED,
-      appGuid: appGuid,
-      app: app
+      appGuid,
+      app
     });
   }
 };
