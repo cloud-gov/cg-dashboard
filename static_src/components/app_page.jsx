3584f3992a050e519bde1d1167ec474a68957d6b
Marco <msegreto@miceover.com>
2016-05-20 16:22:02 -0700

    App page mem and disk display.
    Formatting the bytes format it comes in as megabytes and showing
    both current usage and limit.

--- static_src/components/app_page.jsx
+++ static_src/components/app_page.jsx
@@ -20,46 +20,69 @@ export default class AppPage extends React.Component {
 
   _onChange() {
     this.setState({
-      app: AppStore.get(this.state.currentAppGuid)
+      app: AppStore.get(this.state.currentAppGuid) || {}
     });
   }
 
+  formatMb(bytes) {
+    return Math.round(bytes / 1000000);
+  }
+
   render() {
-    console.log('render app', this.state.app);
+    let content = <h4 className="test-none_message">No app</h4>;
+
+    if (this.state.app.name) {
+      content = (
+        <div>
+          <h2>{ this.state.app.name }</h2>
+          <section>
+            <h3>About</h3>
+            <table>
+              <tbody>
+                <tr>
+                  <td><strong>Name</strong></td>
+                  <td>{ this.state.app.name }</td>
+                </tr>
+                <tr>
+                  <td><strong>State</strong></td>
+                  <td>{ this.state.app.state }</td>
+                </tr>
+                <tr>
+                  <td><strong>Last push</strong></td>
+                  <td>{ this.state.app.package_updated_at }</td>
+                </tr>
+                <tr>
+                  <td><strong>Buildpack</strong></td>
+                  <td>{ this.state.app.buildpack }</td>
+                </tr>
+                <tr>
+                  <td><strong>Memory</strong></td>
+                  <td>
+                    { this.state.app.stats && this.formatMb(
+                        this.state.app.stats.usage.mem) }MB /
+                    { this.state.app.stats &&
+                      this.formatMb(this.state.app.stats.mem_quota) }MB
+                  </td>
+                </tr>
+                <tr>
+                  <td><strong>Disk limit</strong></td>
+                  <td>
+                    { this.state.app.stats && this.formatMb(
+                        this.state.app.stats.usage.disk) }MB /
+                    { this.state.app.stats &&
+                      this.formatMb(this.state.app.stats.disk_quota) }MB
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
+        </div>
+      );
+    }
+
     return (
       <div>
-        <h2>{ this.state.app.name }</h2>
-        <section>
-          <h3>About</h3>
-          <table>
-            <tbody>
-              <tr>
-                <td><strong>Name</strong></td>
-                <td>{ this.state.app.name }</td>
-              </tr>
-              <tr>
-                <td><strong>State</strong></td>
-                <td>{ this.state.app.state }</td>
-              </tr>
-              <tr>
-                <td><strong>Last push</strong></td>
-                <td>{ this.state.app.package_updated_at }</td>
-              </tr>
-              <tr>
-                <td><strong>Buildpack</strong></td>
-                <td>{ this.state.app.buildpack }</td>
-              </tr>
-              <tr>
-                <td><strong>Memory</strong></td>
-                <td>{ this.state.app.memory }</td>
-              </tr>
-              <tr>
-                <td><strong>Disk limit</strong></td>
-                <td>{ this.state.app.disk_quota }</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
+        { content }
       </div>
     );
   }
