447eac9ccdc4f2d49af57eb726b80d2c00124c32
Marco Segreto <marco.segreto@gsa.gov>
2016-05-26 17:19:35 -0700

    RouteStore on actions
    Pretty simple, what happens on fetch and received.

---
+++ static_src/stores/route_store.js
@@ -0,0 +1,44 @@
+
+/*
+ * Store for route data. Will store and update route data on changes from UI and
+ * server.
+ */
+
+import Immutable from 'immutable';
+
+import BaseStore from './base_store.js';
+import cfApi from '../util/cf_api.js';
+import { routeActionTypes } from '../constants.js';
+
+class RouteStore extends BaseStore {
+  constructor() {
+    super();
+    this._data = new Immutable.List();
+    this.subscribe(() => this._registerToActions.bind(this));
+  }
+
+  _registerToActions(action) {
+    switch (action.type) {
+      case routeActionTypes.ROUTES_FOR_APP_FETCH:
+        cfApi.fetchRoutesForApp(action.appGuid);
+        break;
+
+      case routeActionTypes.ROUTES_FOR_APP_RECEIVED: {
+        const routes = action.routes.map((route) =>
+          Object.assign({}, route, { appGuid: action.appGuid })
+        );
+        this.mergeMany('guid', routes, (changed) => {
+          if (changed) this.emitChange();
+        });
+        break;
+      }
+
+      default:
+        break;
+    }
+  }
+}
+
+const _RouteStore = new RouteStore();
+
+export default _RouteStore;
---
+++ static_src/test/unit/stores/route_store.spec.js
@@ -0,0 +1,96 @@
+
+import Immutable from 'immutable';
+
+import '../../global_setup.js';
+
+import AppDispatcher from '../../../dispatcher.js';
+import cfApi from '../../../util/cf_api.js';
+import { wrapInRes, unwrapOfRes } from '../helpers.js';
+import RouteStore from '../../../stores/route_store.js';
+import { routeActionTypes } from '../../../constants';
+
+describe('RouteStore', function() {
+  var sandbox;
+
+  beforeEach(() => {
+    RouteStore._data = Immutable.List();
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('constructor()', function() {
+    it('should start data as empty array', function() {
+      expect(RouteStore.getAll()).toBeEmptyArray();
+    });
+  });
+
+  describe('on route for app fetch', function() {
+    it('should fetch routes for app with app guid from api', function() {
+      var spy = sandbox.spy(cfApi, 'fetchRoutesForApp'),
+          expectedGuid = 'adfasdzcvzxcvb23r';
+
+      AppDispatcher.handleViewAction({
+        type: routeActionTypes.ROUTES_FOR_APP_FETCH,
+        appGuid: expectedGuid
+      });
+
+      expect(spy).toHaveBeenCalledOnce();
+      let arg = spy.getCall(0).args[0];
+      expect(arg).toEqual(expectedGuid);
+    });
+  });
+
+  describe('on route for app received', function() {
+    it('should emit a change event', function() {
+      const appGuid = '2893hazxcmv';
+      const spy = sandbox.spy(RouteStore, 'emitChange');
+
+      AppDispatcher.handleViewAction({
+        type: routeActionTypes.ROUTES_FOR_APP_RECEIVED,
+        appGuid: appGuid,
+        routes: [ { guid: 'adsfa' } ]
+      });
+
+      expect(spy).toHaveBeenCalledOnce();
+    });
+
+    it('should add the appGuid to each route', function() {
+      const sharedGuid = 'zxcb234xcvb4567';
+
+      let routeA = { guid: 'zxcb1234adfg098', host: 'tim' };
+
+      AppDispatcher.handleServerAction({
+        type: routeActionTypes.ROUTES_FOR_APP_RECEIVED,
+        appGuid: sharedGuid,
+        routes: [routeA]
+      });
+
+      let actual = RouteStore.get(routeA.guid);
+
+      expect(actual).toEqual(
+        Object.assign({}, routeA, { appGuid: sharedGuid }));
+    });
+
+    it('should merge all the routes in', function() {
+      const sharedGuid = 'zxcb234nvc654ad';
+      const spy = sandbox.spy(RouteStore, 'mergeMany');
+      const existingRoute = { guid: 'zxcb', appGuid: sharedGuid };
+
+      RouteStore.push(existingRoute);
+
+      const newRoute = { guid: 'zxcb', host: '18f' };
+
+      AppDispatcher.handleServerAction({
+        type: routeActionTypes.ROUTES_FOR_APP_RECEIVED,
+        appGuid: sharedGuid,
+        routes: [newRoute]
+      });
+
+      expect(spy).toHaveBeenCalledOnce();
+      expect(spy).toHaveBeenCalledWith('guid');
+    });
+  });
+});
