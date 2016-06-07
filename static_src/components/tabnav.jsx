4c76df1a540317f67d47359b4769edb1b5df4323
Dan O. Williams <dan.o.williams@gmail.com>
2016-06-07 16:45:46 -0700

    updated template for new tab styles

--- static_src/components/tabnav.jsx
+++ static_src/components/tabnav.jsx
@@ -2,12 +2,16 @@
 import React from 'react';
 
 import classNames from 'classnames';
+import navStyles from 'cloudgov-style/css/components/nav.css';
+import createStyler from '../util/create_styler';
+
 
 export default class Tabnav extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
     this.state = { current: this.props.initialItem };
+    this.styler = createStyler(navStyles);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -15,14 +19,14 @@ export default class Tabnav extends React.Component {
   }
 
   render() {
-    var defaultClasses = ['nav', 'nav-tabs', 'nav-justified'];
+    var defaultClasses = [this.styler('nav'), this.styler('nav-tabs'), this.styler('nav-justified')];
     var classes = classNames(defaultClasses, this.props.classes);
 
     return (
     <ul className={ classes }>
       { this.props.items.map((item, i) => {
         if (item.name === this.state.current) {
-          return <li className="active" key={ i }>{ item.element }</li>;
+          return <li className={ this.styler('active') } key={ i }>{ item.element }</li>;
         } else {
           return <li key={ i }>{ item.element }</li>;
         }
