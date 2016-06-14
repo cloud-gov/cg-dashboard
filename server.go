49546eb17c4b90ab528668d858011807eef92297
Marco Segreto <marco.segreto@gsa.gov>
2016-06-14 12:57:05 -0700

    Adds go apm monitoring to new relic with a plugin
    Uses env vars to hold New Relic License. Will put the browser monitoring
    on top of this apm monitoring.
    
    Tested locally and New Relic did seem to respond although there were
    a bunch of errors about certain features not working due to not being
    supported on Darwin.
    
    Also set the correct env var in the cf -> deck-stage -> cg-deck app
    for the license. I think New Relic should only run there, so will not
    set env vars anywhere else, does that sounds right?

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/18F/cg-deck/controllers/pprof"
 	"github.com/18F/cg-deck/helpers"
 	"github.com/gorilla/context"
+	"github.com/yvasiyarov/gorelic"
 
 	"fmt"
 	"net/http"
@@ -25,6 +26,7 @@ func loadEnvVars() helpers.EnvVars {
 	envVars.LogURL = os.Getenv(helpers.LogURLEnvVar)
 	envVars.PProfEnabled = os.Getenv(helpers.PProfEnabledEnvVar)
 	envVars.BuildInfo = os.Getenv(helpers.BuildInfoEnvVar)
+	envVars.NewRelicLicense = os.Getenv(helpers.NewRelicLicenseEnvVar)
 	return envVars
 }
 
@@ -36,6 +38,15 @@ func main() {
 	}
 	fmt.Println("using port: " + port)
 	startApp(port)
+}
+
+func startMonitoring(license string) {
+	agent := gorelic.NewAgent()
+	agent.Verbose = true
+	agent.CollectHTTPStat = true
+	agent.NewrelicLicense = license
+	agent.NewrelicName = "Cloudgov Deck"
+	agent.Run()
 }
 
 func startApp(port string) {
@@ -53,6 +64,12 @@ func startApp(port string) {
 	if settings.PProfEnabled {
 		pprof.InitPProfRouter(app)
 	}
+
+	if envVars.NewRelicLicense != "" {
+		fmt.Println("starting monitoring...")
+		startMonitoring(envVars.NewRelicLicense)
+	}
+
 	fmt.Println("starting app now...")
 
 	// TODO add better timeout message. By default it will just say "Timeout"
