fb778d5bbb389f509cca0f4e06fa77e86f69769b
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-06-16 10:39:27 -0400

    Mock user_roles and services endpoints
    Add fixture data to simulate the user_roles endpoints on both the org
    and space resource and for the service_instance endpoint. At this
    point, most views are returning data and displaying properly.

--- test/api.js
+++ test/api.js
@@ -2,14 +2,52 @@ var data = require('./fixtures');
 
 var apps = data.apps;
 var organizations = data.organizations;
+var routes = data.routes;
+var serviceInstances = data.serviceInstances;
 var spaces = data.spaces;
+var users = data.users;
 
 var BASE_URL = '/v2';
 
 module.exports = function api(smocks) {
+
+  smocks.route({
+    id: 'app-routes',
+    label: 'App routes',
+    path: `${BASE_URL}/apps/{guid}/routes`,
+    handler: function (req, reply) {
+      var guid = req.params.guid;
+      var route = routes.pop();
+      console.log('guid', guid);
+      reply(route);
+    }
+  });
+
+  smocks.route({
+    id: 'app-stats',
+    label: 'App stats',
+    path: `${BASE_URL}/apps/{guid}/stats`,
+    handler: function (req, reply) {
+      reply({
+        "yo": "fool"
+      });
+    }
+  });
+
+  smocks.route({
+    id: 'app-summary',
+    label: 'App summary',
+    path: `${BASE_URL}/apps/{guid}/summary`,
+    handler: function (req, reply) {
+      var guid = req.params.guid;
+      var app = apps.filter((app) => app.guid === guid).pop();
+      reply(app);
+    }
+  });
+
   smocks.route({
     id: 'organizations',
-    label: 'Organizations', // label is optional
+    label: 'Organizations',
     path: `${BASE_URL}/organizations`,
     handler: function (req, reply) {
       reply({
@@ -19,6 +57,17 @@ module.exports = function api(smocks) {
         "next_url": null,
         "resources": organizations
       })
+    }
+  });
+
+  smocks.route({
+    id: 'organization',
+    label: 'Organization',
+    path: `${BASE_URL}/organizations/{guid}`,
+    handler: function (req, reply) {
+      var guid = req.params.guid;
+      var organization = organizations.filter((org) => org.metadata.guid === guid).pop();
+      reply(organization);
     }
   });
 
@@ -38,6 +87,34 @@ module.exports = function api(smocks) {
   });
 
   smocks.route({
+    id: 'organization-memory-usage',
+    label: 'Organization memory usage',
+    path: `${BASE_URL}/organizations/{guid}/memory_usage`,
+    handler: function (req, reply) {
+      var guid = req.params.guid;
+      reply({
+        memory_usage_in_mb: 17616
+      });
+    }
+  });
+
+  smocks.route({
+    id: 'organization-user-roles',
+    label: 'Organization user roles',
+    path: `${BASE_URL}/organizations/{guid}/user_roles`,
+    handler: function (req, reply) {
+      var guid = req.params.guid;
+      reply({
+        total_results: users.length,
+        total_pages: 1,
+        prev_url: null,
+        next_url: null,
+        resources: users
+      });
+    }
+  });
+
+  smocks.route({
     id: 'space-summary',
     label: 'Space summary',
     path: `${BASE_URL}/spaces/{guid}/summary`,
@@ -47,6 +124,38 @@ module.exports = function api(smocks) {
         guid: guid,
         name: `space-${guid}`,
         apps: apps
+      });
+    }
+  });
+
+  smocks.route({
+    id: 'space-service-instances',
+    label: 'Space service instances',
+    path: `${BASE_URL}/spaces/{guid}/service_instances`,
+    handler: function (req, reply) {
+      var guid = req.params.guid;
+      reply({
+        total_results: serviceInstances.length,
+        total_pages: 1,
+        prev_url: null,
+        next_url: null,
+        resources: serviceInstances
+     });
+   }
+  });
+
+  smocks.route({
+    id: 'space-user-roles',
+    label: 'Space user roles',
+    path: `${BASE_URL}/spaces/{guid}/user_roles`,
+    handler: function (req, reply) {
+      var guid = req.params.guid;
+      reply({
+        total_results: users.length,
+        total_pages: 1,
+        prev_url: null,
+        next_url: null,
+        resources: users
       });
     }
   });
--- test/fixtures.js
+++ test/fixtures.js
@@ -4,9 +4,49 @@ var appGuids = [
   'app-guid-one',
   'app-guid-two',
   'app-guid-three'
-]
+];
 
 module.exports.appGuids = appGuids;
+
+var organizationGuids = [
+  'org-guid-one',
+  'org-guid-two',
+  'org-guid-three'
+];
+
+module.exports.organizationGuids = organizationGuids;
+
+var routeGuids = [
+  'route-guid-one',
+  'route-guid-two',
+  'route-guid-three'
+];
+
+module.exports.routeGuids = routeGuids;
+
+var serviceInstanceGuids = [
+  'service-instance-guid-one',
+  'service-instance-guid-two',
+  'service-instance-guid-three'
+];
+
+module.exports.serviceInstanceGuids = serviceInstanceGuids;
+
+var spaceGuids = [
+  'space-guid-one',
+  'space-guid-two',
+  'space-guid-three'
+];
+
+module.exports.spaceGuids = spaceGuids;
+
+var userGuids = [
+  'user-guid-one',
+  'user-guid-two',
+  'user-guid-three'
+];
+
+module.exports.userGuids = userGuids;
 
 var apps = appGuids.map(function(guid) {
   return {
@@ -47,14 +87,6 @@ var apps = appGuids.map(function(guid) {
 
 module.exports.apps = apps;
 
-var organizationGuids = [
-  'org-guid-one',
-  'org-guid-two',
-  'org-guid-three'
-];
-
-module.exports.organizationGuids = organizationGuids;
-
 var organizations = organizationGuids.map(function(guid) {
   return {
     metadata: {
@@ -84,13 +116,75 @@ var organizations = organizationGuids.map(function(guid) {
 
 module.exports.organizations  = organizations;
 
-var spaceGuids = [
-  'space-guid-one',
-  'space-guid-two',
-  'space-guid-three'
-];
+var routes = routeGuids.map(function(guid, i){
+  var domainGuid = 'yo';
+  var spaceGuid = spaceGuids[i];
+  return {
+    total_results: 1,
+    total_pages: 1,
+    prev_url: null,
+    next_url: null,
+    resources: [
+      {
+        metadata: {
+          guid: guid,
+          url: `${URL_BASE}/routes/${guid}`,
+          created_at: '2015-10-13T18:30:37Z',
+          updated_at: null
+        },
+        entity: {
+          host: 'console',
+          path: '',
+          domain_guid: '',
+          space_guid: spaceGuids[i],
+          service_instance_guid: null,
+          port: 0,
+          domain_url: `${URL_BASE}/domains/${domainGuid}`,
+          space_url: `${URL_BASE}/spaces/${spaceGuid}`,
+          apps_url: `${URL_BASE}/routes/${guid}/apps`,
+          route_mappings_url: `${URL_BASE}/routes/${guid}/route_mappings`
+        }
+      }
+    ]
+  }
+});
 
-module.exports.spaceGuids = spaceGuids;
+module.exports.routes = routes;
+
+var serviceInstances = serviceInstanceGuids.map(function(guid, i) {
+  return {
+    metadata: {
+      guid: guid,
+      url: `${URL_BASE}/service_instances/${guid}`,
+      created_at: '2015-07-14T04:02:30Z',
+      updated_at: null
+    },
+    entity: {
+      name: `service-instance-${guid}`,
+      credentials: {},
+      service_plan_guid: 'fake-service-plan-guid',
+      space_guid: spaceGuids[i],
+      gateway_data: null,
+      dashboard_url: null,
+      type: 'managed_service_instance',
+      last_operation: {
+        type: 'create',
+        state: 'succeeded',
+        description: 'The instance was created',
+        created_at: '2015-07-14T04:02:30Z',
+        updated_at: '2015-07-14T04:02:30Z',
+        tags: [],
+        space_url: `${URL_BASE}/spaces/${spaceGuids[i]}`,
+        service_plan_url: '',
+        service_bindings_url: `${URL_BASE}/service_instances/${guid}/service_bindings`,
+        service_keys_url: `${URL_BASE}/service_instances/${guid}/service_keys`,
+        routes_url: `${URL_BASE}/service_instances/${guid}/routes`
+      }
+    }
+  }
+});
+
+module.exports.serviceInstances = serviceInstances;
 
 var spaces = spaceGuids.map(function(guid){
   return {
@@ -104,3 +198,30 @@ var spaces = spaceGuids.map(function(guid){
 });
 
 module.exports.spaces = spaces;
+
+var users = userGuids.map(function(guid, i) {
+  return {
+    metadata: {
+      guid: guid,
+      url: `${URL_BASE}/users/${guid}`,
+      created_at: "2015-02-19T08:46:28Z",
+      updated_at: null
+    },
+    entity: {
+      admin: (i === 0) ? true : false,
+      active: true,
+      default_space_guid: null,
+      username: `user-${guid}`,
+      organization_roles: [ 'org_user' ],
+      spaces_url: `${URL_BASE}/users/${guid}/spaces`,
+      organizations_url: `${URL_BASE}/users/${guid}/organizations`,
+      managed_organizations_url: `${URL_BASE}/users/${guid}/managed_organizations_url`,
+      billing_managed_organizations_url: `${URL_BASE}/users/${guid}/billing_managed_organizations`,
+      audited_organizations_url: `${URL_BASE}/users/${guid}/audited_organizations`,
+      managed_spaces_url: `${URL_BASE}/users/${guid}/managed_spaces`,
+      audited_spaces_url: `${URL_BASE}/users/${guid}/audited_spaces`
+    }
+  }
+});
+
+module.exports.users = users;
