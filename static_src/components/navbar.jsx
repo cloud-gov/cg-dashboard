839f58673190dde88064db69792f00fec2313a24
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-07-12 00:25:58 -0400

    Remove commented line

--- static_src/components/navbar.jsx
+++ static_src/components/navbar.jsx
@@ -49,7 +49,6 @@ export class Nav extends React.Component {
   }
 
   _handleOverviewClick() {
-    // orgActions.changeCurrentOrg('0');
     orgActions.toggleSpaceMenu('0');
     spaceActions.changeCurrentSpace('0');
   }
