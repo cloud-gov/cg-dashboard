8385cfc368d85ba63f26689279c3cb8c8b5690d0
Marco <msegreto@miceover.com>
2016-07-29 10:48:22 -0700

    Specify unit folder for unit tests
    In a weird attempt to fix the test failures that don't make sense

--- static_src/tests.bundle.js
+++ static_src/tests.bundle.js
@@ -1,3 +1,3 @@
-var context = require.context('./test', true, /.+\.spec\.jsx?$/);
+var context = require.context('./test/unit', true, /.+\.spec\.jsx?$/);
 context.keys().forEach(context);
 module.exports = context;
