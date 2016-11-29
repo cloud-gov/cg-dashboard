7bf5bf68451ae37864638f1de3c277279878936f
Aaron D Borden <adborden@a14n.net>
2016-11-28 23:23:37 -0800

    Use state to track allocation

--- static_src/components/stat.jsx
+++ static_src/components/stat.jsx
@@ -17,7 +17,7 @@ const STATES = [
 const propTypes = {
   name: React.PropTypes.string,
   editable: React.PropTypes.bool,
-  primaryStat: React.PropTypes.string.isRequired,
+  primaryStat: React.PropTypes.number.isRequired,
   statState: React.PropTypes.oneOf(STATES),
   secondaryInfo: React.PropTypes.element
 };
@@ -28,23 +28,42 @@ const defaultProps = {
   secondaryInfo: <span></span>
 };
 
+function stateSetter(props) {
+  return {
+    primaryStat: props.primaryStat
+  };
+}
+
 export default class Stat extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = stateSetter(props);
     this.styler = createStyler(style);
+    this._onChange = this._onChange.bind(this);
+  }
+
+  _onChange(e) {
+    this.setState(stateSetter({ primaryStat: e.target.value }));
   }
 
   render() {
     const stateClass = `stat-${this.props.statState}`;
     let primaryStat = (
       <span className={ this.styler('stat-primary')}>
-	{ formatBytes(this.props.primaryStat) }
+	{ formatBytes(this.state.primaryStat) }
       </span>
     );
 
     if (this.props.editable) {
-      primaryStat = <input type="text" name={ this.props.name } value={ this.props.primaryStat } />;
+      primaryStat = (
+        <input
+          type="text"
+          id={ this.props.name }
+          name={ this.props.name }
+          value={ this.state.primaryStat }
+          onChange={ this._onChange }
+        />
+      );
     }
 
     return (
