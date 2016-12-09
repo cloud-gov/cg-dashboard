a7a2de8aca7c2a3e42ffca717f85ab067a6aa423
Marco <msegreto@miceover.com>
2016-12-09 13:45:04 -0800

    Remove other pages and tabnav from space page

--- static_src/components/space_container.jsx
+++ static_src/components/space_container.jsx
@@ -1,6 +1,5 @@
 
 import React from 'react';
-import style from 'cloudgov-style/css/cloudgov-style.css';
 
 import AppList from '../components/app_list.jsx';
 import OrgStore from '../stores/org_store.js';
@@ -9,7 +8,9 @@ import SpaceStore from '../stores/space_store.js';
 import Users from './users.jsx';
 import Tabnav from './tabnav.jsx';
 import TabnavItem from './tabnav_item.jsx';
+import { config } from 'skin';
 import createStyler from '../util/create_styler';
+import style from 'cloudgov-style/css/cloudgov-style.css';
 
 
 const PAGES = {
@@ -58,27 +59,6 @@ export default class SpaceContainer extends React.Component {
     return `/#/org/${this.state.currentOrg.guid}/spaces/${this.state.space.guid}/${page}`;
   }
 
-  get currentContent() {
-    return PAGES[this.props.currentPage];
-  }
-
-  get subNav() {
-    return [
-      { name: 'apps',
-        element: <TabnavItem controls="apps" href={this.spaceUrl('apps')}
-          content="Apps" />
-      },
-      { name: 'services',
-        element: <TabnavItem controls="services" href={this.spaceUrl('services')}
-          content="Service Instances" />
-      },
-      { name: 'users',
-        element: <TabnavItem controls="users" href={this.spaceUrl('users')}
-          content="User Management" />
-      }
-    ];
-  }
-
   get currentOrgName() {
     return this.state.currentOrg ? this.state.currentOrg.name : '';
   }
@@ -91,30 +71,18 @@ export default class SpaceContainer extends React.Component {
     let Content = this.currentContent;
     let tabNav = <div></div>;
     let main = <div></div>;
+    console.log(this.state);
 
     if (this.state.space && this.state.space.guid) {
-      if (this.state.currentOrg) {
-        tabNav = (
-          <Tabnav items={ this.subNav } initialItem={ this.props.currentPage } />
-        );
-      }
       main = (
-      <div>
-        <div>
-          <h2>
-            <strong>{this.state.space.name}</strong> space in your <strong>{ this.currentOrgName }</strong> organization
-          </h2>
+      <div className={ this.styler('grid') }>
+        <div className={ this.styler('grid-width-8') }>
+          <h2>Space overview</h2>
           <p className={ this.styler('page-dek') }>
             Each <a href="https://docs.cloud.gov/getting-started/concepts/">space</a> provides an environment for related applications (<a href="https://docs.cloud.gov/intro/overview/using-cloudgov-paas/">example use</a>).
           </p>
         </div>
-        { tabNav }
-        <div>
-          <div role="tabpanel" id={ this.props.currentPage }>
-            <Content
-              intitialApps={ this.state.space.apps || [] }
-            />
-          </div>
+        <div className={ this.styler('grid-width-4') }>
         </div>
       </div>
       );
