b7357404041acf0ae57c29be1b1619f10bc069ef
Aaron D Borden <adborden@a14n.net>
2017-01-20 17:41:47 -0800

    Add pageobject for testing org quicklook

--- static_src/test/functional/overview_quicklook.spec.js
+++ static_src/test/functional/overview_quicklook.spec.js
@@ -1,9 +1,50 @@
 
 import './global_setup';
 
+import OrgQuicklookElement from './pageobjects/org_quicklook.element';
+
+
 describe('Overview page', function () {
   it('navigates to page', function () {
     browser.url('/');
+  });
+
+  it('has a title', function () {
     expect(browser.getTitle()).toBe('cloud.gov dashboard');
+  });
+
+  it('has a page header', function () {
+    const pageHeader = browser.element('.page-header-title');
+    expect(pageHeader.getText()).toBe('Overview');
+  });
+
+  describe('quicklook', function () {
+    let quicklookElement;
+
+    it('exists', function () {
+      quicklookElement = new OrgQuicklookElement(browser, browser.element('.panel-row-boxed'));
+      expect(quicklookElement.isVisible()).toBe(true);
+    });
+
+    it('has org name', function () {
+      const orgName = quicklookElement.title();
+      expect(orgName).toBe('fake-cf');
+    });
+
+    it('is collapsed', function () {
+      expect(quicklookElement.isExpanded()).toBe(false);
+    });
+
+    it('is clicked', function () {
+      quicklookElement.click();
+    });
+
+    it('is expanded', function () {
+      expect(quicklookElement.isExpanded()).toBe(true);
+    });
+
+    it('has 2 rows', function () {
+      expect(quicklookElement.rows().length).toBe(2);
+    });
   });
 });
