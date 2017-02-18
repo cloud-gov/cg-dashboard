bb07f3c3b8f5423246a57f2b924ffa2e10c69a59
Aaron D Borden <adborden@a14n.net>
2017-02-17 16:59:12 -0800

    UsageAndLimits should be wrapped in Form

--- static_src/components/stat.jsx
+++ static_src/components/stat.jsx
@@ -17,6 +17,7 @@ const STATES = [
 ];
 
 const propTypes = {
+  formGuid: React.PropTypes.string,
   name: React.PropTypes.string,
   title: React.PropTypes.string,
   editable: React.PropTypes.bool,
@@ -106,15 +107,16 @@ export default class Stat extends React.Component {
           <FormNumber
             className={ this.styler('stat-input', 'stat-input-text') }
             type="text"
-            id={ `${this.props.name}-value` }
+            id={ this.props.name }
             inline
             label="MB"
             labelAfter
-            name={ `${this.props.name}-value` }
+            name={ this.props.name }
             value={ this.fromBytes(this.state.primaryStat) }
             min={ this.props.min }
             max={ this.props.max }
             onValidate={ this.onValidate }
+            formGuid={ this.props.formGuid }
           />
         </div>
       );
