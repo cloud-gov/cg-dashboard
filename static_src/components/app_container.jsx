99d3203f7f9925a5a407eeb6354593e035c62548
Marco <msegreto@miceover.com>
2017-03-02 21:02:26 -0800

    Move logs message to activity log container
    Based on things found in usability testing.

--- static_src/components/app_container.jsx
+++ static_src/components/app_container.jsx
@@ -206,7 +206,6 @@ export default class AppContainer extends React.Component {
             </div>
           </div>
           <Panel title="Usage and allocation">
-            { this.logsDocumentation }
             <UsageLimits app={ this.state.app } quota={ this.state.quota } />
           </Panel>
 
@@ -219,6 +218,7 @@ export default class AppContainer extends React.Component {
           </Panel>
 
           <Panel title="Recent activity">
+            { this.logsDocumentation }
             <ActivityLog initialAppGuid={ this.state.app.guid } />
           </Panel>
         </div>
