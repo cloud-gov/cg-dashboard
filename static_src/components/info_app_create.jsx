bc15ee9cac28f7820e70f8196d815542752e4826
Marco <msegreto@miceover.com>
2017-03-20 19:08:36 -0700

    Add brief empty states for space quicklooks
    Which will appear on overview pages, space pages etc.

--- static_src/components/info_app_create.jsx
+++ static_src/components/info_app_create.jsx
@@ -8,10 +8,12 @@ import createStyler from '../util/create_styler';
 
 const propTypes = {
   org: React.PropTypes.object,
-  space: React.PropTypes.object
+  space: React.PropTypes.object,
+  brief: React.PropTypes.bool
 };
 
 const defaultProps = {
+  brief: false
 };
 
 
@@ -24,9 +26,30 @@ export default class InfoAppCreate extends React.Component {
   render() {
     const org = this.props.org || {};
     const space = this.props.space || {};
+    let content;
 
-    return (
-      <div className={ this.styler('info', 'info-app_create') }>
+    if (this.props.brief) {
+      const cliLink = config.docs.cli ?
+        <a href={ config.docs.cli }>command line interface (CLI)</a> :
+        <span>Command line interface (CLI)</span>;
+      const deployingApps = config.docs.deploying_apps &&
+        <a href={ config.docs.deploying_apps}>Read more about adding apps</a>;
+
+      content = (
+      <div>
+        <p>
+          Add a new app to this space in the { cliLink }
+        </p>
+        <pre>
+          <code>$ cf target -o { org.name || '<org>' } -s { space.name || '<space>' }</code><br />
+          <code>$ cf push my-app</code>
+        </pre>
+        { deployingApps }
+      </div>
+      );
+    } else {
+      content = (
+      <div>
         <p>
           Learn how to <a href={ config.docs.deploying_apps }>deploy a new app</a>.
         </p>
@@ -56,6 +79,13 @@ export default class InfoAppCreate extends React.Component {
           <code>$ cf target -o { org.name || '<org>' } -s { space.name || '<space>' }</code><br />
           <code>$ cf push my-app</code>
         </pre>
+      </div>
+      );
+    }
+
+    return (
+      <div className={ this.styler('info', 'info-app_create') }>
+        { content }
       </div>
     );
   }
