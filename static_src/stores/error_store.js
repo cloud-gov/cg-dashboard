6c2bff1d38068c3dc03592a981882114408a641c
Marco Segreto <marco.segreto@gsa.gov>
2017-03-27 16:09:05 -0700

    Modify global errors to display just 1
    Because user's were often reporting angst and confusion when more
    then one error was appearing.

--- static_src/stores/error_store.js
+++ static_src/stores/error_store.js
@@ -11,7 +11,7 @@ import { errorActionTypes } from '../constants.js';
 export class ErrorStore extends BaseStore {
   constructor() {
     super();
-    this.maxErrors = 5;
+    this.maxErrors = 2;
     this.subscribe(() => this._registerToActions.bind(this));
   }
 
