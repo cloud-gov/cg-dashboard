aaefa14d59d239eff651559023e8b92c1109dbc2
James Hupp <james.hupp@gsa.gov>
2017-03-29 13:01:54 -0400

    Link to sandbox info from cheatsheet
    Giving people access to policies

--- static_src/components/info_sandbox.jsx
+++ static_src/components/info_sandbox.jsx
@@ -15,6 +15,7 @@ export default class InfoSandbox extends React.Component {
       <section className={ this.props.className }>
         <h4>Looking at an empty sandbox?</h4>
         <p><a href="https://cloud.gov/docs/getting-started/your-first-deploy/">Try making a “hello world” app</a>.</p>
+        <p>Then see <a href="https://cloud.gov/overview/pricing/free-limited-sandbox/#a-few-things-you-can-try-in-your-sandbox">what else you can do</a> and the <a href="https://cloud.gov/overview/pricing/free-limited-sandbox/#sandbox-limitations">sandbox policies</a>.</p>
       </section>
     );
   }
