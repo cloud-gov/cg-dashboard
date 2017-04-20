4af99901dff4e31955936ad163a3556828b86f1d
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-04-20 17:55:28 -0400

    Create the fixture for user_roles

---
+++ static_src/test/server/fixtures/user_roles.js
@@ -0,0 +1,60 @@
+
+const userRoles = {
+  "space_manager_space_xx": {
+    "user_id": "space-manager-xx-uid-601d-48c4-9705",
+    "sub": "space-manager-xx-uid-601d-48c4-9705",
+    "user_name": "fake-space-manager-XX@gsa.gov",
+    "given_name": "fake-space-manager-XX",
+    "family_name": "gsa.gov",
+    "email": "fake-space-manager-XX@gsa.gov",
+    "phone_number": null,
+    "previous_logon_time": 1489612053883,
+    "name": "fake-space-manager-XX@gsa.gov"
+  },
+  "space_manager_space_yy": {
+    "user_id": "space-manager-yy-uid-601d-48c4-9705",
+    "sub": "space-manager-yy-uid-601d-48c4-9705",
+    "user_name": "fake-space-manager-YY@gsa.gov",
+    "given_name": "fake-space-manager-YY",
+    "family_name": "gsa.gov",
+    "email": "fake-space-manager-YY@gsa.gov",
+    "phone_number": null,
+    "previous_logon_time": 1489612053883,
+    "name": "fake-space-manager-YY@gsa.gov"
+  },
+  "org_manager_space_x": {
+    "user_id": "org-manager-x-uid-601d-48c4-9705",
+    "sub": "org-manager-x-uid-601d-48c4-9705",
+    "user_name": "fake-org-manager-X@gsa.gov",
+    "given_name": "fake-org-manager-X",
+    "family_name": "gsa.gov",
+    "email": "fake-org-manager-X@gsa.gov",
+    "phone_number": null,
+    "previous_logon_time": 1489612053883,
+    "name": "fake-org-manager-X@gsa.gov"
+  },
+  "org_manager_space_y": {
+    "user_id": "org-manager-y-uid-601d-48c4-9705",
+    "sub": "org-manager-y-uid-601d-48c4-9705",
+    "user_name": "fake-org-manager-Y@gsa.gov",
+    "given_name": "fake-org-manager-Y",
+    "family_name": "gsa.gov",
+    "email": "fake-org-manager-Y@gsa.gov",
+    "phone_number": null,
+    "previous_logon_time": 1489612053883,
+    "name": "fake-org-manager-Y@gsa.gov"
+  },
+  "default": {
+    "user_id": "bba7537f-601d-48c4-9705-4583ba54ea4b",
+    "sub": "bba7537f-601d-48c4-9705-4583ba54ea4b",
+    "user_name": "fake-personb@gsa.gov",
+    "given_name": "fake-personb",
+    "family_name": "gsa.gov",
+    "email": "fake-personb@gsa.gov",
+    "phone_number": null,
+    "previous_logon_time": 1489612053883,
+    "name": "fake-personb@gsa.gov"
+  }
+};
+
+module.exports = userRoles;
