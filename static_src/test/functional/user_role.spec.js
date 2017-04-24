44486e42b21edee8b430e3513fa88b76f1db6adc
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-04-24 11:08:53 -0400

    Save the working org test

--- static_src/test/functional/user_role.spec.js
+++ static_src/test/functional/user_role.spec.js
@@ -11,7 +11,6 @@ describe('User roles', function () {
   });
 
   describe('User role cookie test', function () {
-
     describe('when cookie is set and deleted', function () {
       it('should reflect the cookie content', function () {
         browser.setCookie({ name: 'testing_user_role', value: 'space_manager_space_xx' });
@@ -23,28 +22,26 @@ describe('User roles', function () {
   });
 
   describe('Org page', function () {
-    it('navigates to a org page', function () {
-      browser.url('/#/org/user_role-org_x-ffe7-4aa8-8e85-94768d6bd250');
-    });
-
     it('has a title', function () {
+      browser.url('/#/org/user_role-org_x-ffe7-4aa8-8e85-94768d6bd250');
       expect(browser.getTitle()).toBe('cloud.gov dashboard');
     });
 
     it('has a page header', function () {
+      browser.url('/#/org/user_role-org_x-ffe7-4aa8-8e85-94768d6bd250');
       const pageHeader = browser.element('.test-page-header-title');
       expect(pageHeader.getText()).toBe('fake-cf-user_role-org_x-testing');
     });
 
     describe('org manager for org X then they should', function () {
-  //     it('be able to edit roles for org X', function () {
-  //       browser.setCookie({ name: 'testing_user_role', value: 'org_manager_org_x' });
-  //       browser.url('/uaa/userinfo');
-  //       cookieResult = browser.getCookie('testing_user_role').value;
-  //       expect(cookieResult).toBe('org_manager_org_x');
+      it('be able to edit roles for org X', function () {
+        browser.setCookie({ name: 'testing_user_role', value: 'org_manager_org_x' });
+        browser.url('/');
+        cookieResult = browser.getCookie('testing_user_role').value;
+        expect(cookieResult).toBe('org_manager_org_x');
 
-  //       expect(true).toBe(true);
-  //     });
+        expect(true).toBe(true);
+      });
 
       it('not be able to edit roles for org Y', function () {
         expect(true).toBe(true);
@@ -58,7 +55,7 @@ describe('User roles', function () {
     });
   });
 
-  // describe('Space page', function () {
+  describe('Space page', function () {
   //   it('navigates to a org\'s space page', function () {
   //     const orgGuid = '48b3f8a1-ffe7-4aa8-8e85-94768d6bd250';
   //     const spaceGuid = '82af0edb-8540-4064-82f2-d74df612b794';
@@ -88,5 +85,5 @@ describe('User roles', function () {
   //       expect(true).toBe(true);
   //     });
   //   });
-  // });
+  });
 });
