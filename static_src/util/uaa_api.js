204c8485526d58405826c851da825e619377398c
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-05-01 17:31:10 -0400

    Add the uaa api action that does the invite action

--- static_src/util/uaa_api.js
+++ static_src/util/uaa_api.js
@@ -12,5 +12,17 @@ export default {
   fetchUaaInfo(guid) {
     return http.get(`${URL}/uaainfo?uaa_guid=${guid}`)
       .then(res => res.data);
+  },
+
+  // checkForUaaUser(email) {
+  //   return http.post(`${URL}/check_for_user`)
+  //     .then(res => res.data);
+  // },
+
+  inviteUaaUser(email) {
+    return http.post(`${URL}/invite_users?redirect_uri=https://dashboard.fr.cloud.gov/`, {
+    "emails": [ email ]
+  })
+      .then(res => res.data);
   }
 };
