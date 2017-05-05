55e0cafca6a3f5c489febe40097d781cc051917d
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-05-05 12:16:31 -0400

    Recover and pass back for email request

--- static_src/util/uaa_api.js
+++ static_src/util/uaa_api.js
@@ -15,9 +15,42 @@ export default {
   },
 
   inviteUaaUser(email) {
+    var self = this;
     return http.post(`${URL}/invite/users/create`, {
       emails: [email]
     })
-      .then(res => res.data);
+      .then(function (res){
+        self.initiateInviteEmail(res.data);
+      });
+  },
+
+  // Example response:
+  // {
+  //  "new_invites": [
+  //    {
+  //      "email":"example11@domkain.com",
+  //      "userId":"959f1c8e-e615-4a73-80a6-ce6751c00719",
+  //      "origin":"uaa",
+  //      "success":true,
+  //      "errorCode":null,
+  //      "errorMessage":null,
+  //      "inviteLink":"https://uaa.fr.cloud.gov/invitations/accept?code=dd8ef6b5"
+  //    }
+  //  ],
+  //  "failed_invites":[]
+  // }
+  initiateInviteEmail(response) {
+    if (response['new_invites'].length > 0){
+      let email, invite_url;
+      email = response['new_invites'][0]['email']
+      invite_url = response['new_invites'][0]['inviteLink']
+      if (email && invite_url){
+        return http.get(`${URL}/invite/email/send?email=${email}&invite_url=${invite_url}`, {
+          emails: [email]
+        })
+          .then(res => res.data);
+      }
+    }
+    return false;
   }
 };
