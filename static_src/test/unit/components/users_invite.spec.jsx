0f4d3043003f3fba8199824259a1a0c1dd030744
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-05-11 15:33:13 -0400

    Comment out stubs when not being used

--- static_src/test/unit/components/users_invite.spec.jsx
+++ static_src/test/unit/components/users_invite.spec.jsx
@@ -1,19 +1,19 @@
 import '../../global_setup.js';
 
-import React from 'react';
-import { shallow } from 'enzyme';
+// import React from 'react';
+// import { shallow } from 'enzyme';
 
-import { Form, FormText } from '../../../components/form';
-import PanelDocumentation from '../../../components/panel_documentation.jsx';
-import Action from '../../../components/action.jsx';
+// import { Form, FormText } from '../../../components/form';
+// import PanelDocumentation from '../../../components/panel_documentation.jsx';
+// import Action from '../../../components/action.jsx';
 
-import FormStore from '../../stores/form_store';
+// import FormStore from '../../stores/form_store';
 
-import userActions from '../../actions/user_actions';
+// import userActions from '../../actions/user_actions';
 
-import { validateString } from '../../util/validators';
+// import { validateString } from '../../util/validators';
 
-const USERS_INVITE_FORM_GUID = 'users-invite-form';
+// const USERS_INVITE_FORM_GUID = 'users-invite-form';
 
 describe('<UsersInvite />', function () {
   let sandbox;
