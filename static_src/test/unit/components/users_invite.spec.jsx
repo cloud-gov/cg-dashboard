c9f342d4b10f700fb212881997184673c652a2c9
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-05-12 17:38:20 -0400

    Create the user invite component and stub tests

---
+++ static_src/test/unit/components/users_invite.spec.jsx
@@ -0,0 +1,28 @@
+import '../../global_setup.js';
+
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import { Form, FormText } from '../../../components/form';
+// import PanelDocumentation from '../../../components/panel_documentation.jsx';
+// import Action from '../../../components/action.jsx';
+
+// import FormStore from '../../stores/form_store';
+
+// import userActions from '../../actions/user_actions';
+
+// import { validateString } from '../../util/validators';
+
+// const USERS_INVITE_FORM_GUID = 'users-invite-form';
+
+describe('<UsersInvite />', function () {
+  let sandbox;
+
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+});
