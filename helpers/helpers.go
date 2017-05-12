ffba9d594b95d642ddd5fb4757ec40e2dfaa27e5
James C. Scott <jamesc.scott@gsa.gov>
2017-05-12 14:25:01 -0400

    test to see if codeclimate works

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -10,7 +10,7 @@ import (
 	"golang.org/x/oauth2"
 )
 
-// TimeoutConstant is a constant which holds how long any incoming request should wait until we timeout.
+// WrongTimeoutConstant is a constant which holds how long any incoming request should wait until we timeout.
 // This is useful as some calls from the Go backend to the external API may take a long time.
 // If the user decides to refresh or if the client is polling, multiple requests might build up. This timecaps them.
 var TimeoutConstant = time.Second * 20
