5232988569a4ebfe1bf552448729c9a2924a054a
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-05-16 15:49:59 -0400

    Create a test that checks for form object in user invite block

--- static_src/test/unit/components/users_invite.spec.jsx
+++ static_src/test/unit/components/users_invite.spec.jsx
@@ -1,28 +1,29 @@
 import '../../global_setup.js';
 
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import { Form, FormText } from '../../../components/form';
-// import PanelDocumentation from '../../../components/panel_documentation.jsx';
-// import Action from '../../../components/action.jsx';
+import { Form } from '../../../components/form';
+import UsersInvite from '../../../components/users_invite.jsx';
 
-// import FormStore from '../../stores/form_store';
+import FormStore from '../../stores/form_store';
 
-// import userActions from '../../actions/user_actions';
-
-// import { validateString } from '../../util/validators';
-
-// const USERS_INVITE_FORM_GUID = 'users-invite-form';
+const USERS_INVITE_FORM_GUID = 'users-invite-form';
 
 describe('<UsersInvite />', function () {
   let sandbox;
 
   beforeEach(function () {
     sandbox = sinon.sandbox.create();
+    FormStore.create(USERS_INVITE_FORM_GUID);
   });
 
   afterEach(function () {
     sandbox.restore();
+  });
+
+  it('renders one <Form /> components', () => {
+    const userInvite = shallow(<UsersInvite />);
+    expect(userInvite.find(Form)).to.have.length(1);
   });
 });
