94ce171a47f83a3928a0bf444ac11fad95c54bf5
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-05-16 21:30:23 -0400

    Create functional test to interact with the form without submitting anything

---
+++ static_src/test/functional/pageobjects/user_invite.element.js
@@ -0,0 +1,27 @@
+
+import BaseElement from './base.element';
+
+// https://www.martinfowler.com/bliki/PageObject.html
+//
+// Represents a UserRoleElement for making assertions against. This makes it
+// easier to abstract some of the webdriver details from the UI component.
+
+// TODO attach to class as static property
+
+export default class UserInviteElement extends BaseElement {
+  inputToInviteForm(input) {
+    return this.element('.test-users_invite_name').setValue(input);
+  }
+
+  getInviteFormValue() {
+    return this.element('.test-users_invite_name').getValue();
+  }
+
+  submitInviteForm() {
+    return this.element('.users_invite_form').submit();
+  }
+
+  countNumberOfUsers() {
+    return this.elements('.complex_list-item').length
+  }
+}
---
+++ static_src/test/functional/user_invite.spec.js
@@ -0,0 +1,42 @@
+
+import UserInviteElement from './pageobjects/user_invite.element';
+import UserRoleElement from './pageobjects/user_role.element';
+
+describe('User roles', function () {
+  let userInviteElement,
+      numberOfUsers,
+      userRoleElement;
+
+  const guidManagerOrgX = 'org-manager-x-uid-601d-48c4-9705',
+        email = 'name@domain.com',
+        cookieManagerOrgX = 'org_manager_org_x',
+        urlOrgX = '/#/org/user_role-org_x-ffe7-4aa8-8e85-94768d6bd250';
+
+  describe('A user on page for an org', function () {
+    it('should navigates to org X', function () {
+      browser.url(urlOrgX);
+
+      userRoleElement = new UserRoleElement(browser, browser.element('.test-users'));
+      userRoleElement.setAndGetUserRole(cookieManagerOrgX);
+
+      browser.waitForExist('.test-users');
+      expect(browser.isExisting('.test-users')).toBe(true);
+    });
+
+    it('should have the user invite panel', function () {
+      userInviteElement = new UserInviteElement(browser, browser.element('.test-users-invite'));
+      browser.waitForExist('.test-users-invite');
+
+      expect(userInviteElement.isVisible()).toBe(true);
+    });
+
+    it('should get submission response from server', function () {
+      numberOfUsers = userInviteElement.countNumberOfUsers();
+    });
+
+    it('should be able to input content into invite form', function () {
+      userInviteElement.inputToInviteForm(email);
+      expect(email).toBe(userInviteElement.getInviteFormValue());
+    });
+  });
+});
