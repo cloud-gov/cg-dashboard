9a6cb87e8e8a34266bf2a133a705bbd9422d3a13
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-06-08 14:21:14 -0400

    Add missing roles to the fixture

--- static_src/test/server/fixtures/user_roles.js
+++ static_src/test/server/fixtures/user_roles.js
@@ -22,6 +22,28 @@ const userRoles = {
     "previous_logon_time": 1489612053883,
     "name": "fake-org-x-space-manager-YY@gsa.gov"
   },
+  "org_y_space_manager_space_xx": {
+    "user_id": "org-y-space-manager-xx-uid-601d-48c4-9705",
+    "sub": "org-y-space-manager-xx-uid-601d-48c4-9705",
+    "user_name": "fake-org-y-space-manager-XX@gsa.gov",
+    "given_name": "fake-org-y-space-manager-XX",
+    "family_name": "gsa.gov",
+    "email": "fake-org-y-space-manager-XX@gsa.gov",
+    "phone_number": null,
+    "previous_logon_time": 1489612053883,
+    "name": "fake-org-y-space-manager-XX@gsa.gov"
+  },
+  "org_y_space_manager_space_yy": {
+    "user_id": "org-y-space-manager-yy-uid-601d-48c4-9705",
+    "sub": "org-y-space-manager-yy-uid-601d-48c4-9705",
+    "user_name": "fake-org-y-space-manager-YY@gsa.gov",
+    "given_name": "fake-org-y-space-manager-YY",
+    "family_name": "gsa.gov",
+    "email": "fake-org-y-space-manager-YY@gsa.gov",
+    "phone_number": null,
+    "previous_logon_time": 1489612053883,
+    "name": "fake-org-y-space-manager-YY@gsa.gov"
+  },
   "org_manager_org_x": {
     "user_id": "org-manager-x-uid-601d-48c4-9705",
     "sub": "org-manager-x-uid-601d-48c4-9705",
