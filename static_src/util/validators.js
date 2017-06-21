ac705b1ba894100ee8b16674d147c650044ed20e
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-06-21 15:42:53 -0400

    Use template

--- static_src/util/validators.js
+++ static_src/util/validators.js
@@ -42,7 +42,7 @@ export function validateString() {
 export function validateEmail() {
   return function _validateEmail(value, name) {
     if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value))) {
-      const nameString = name ? 'in ' + name + ' ' : '';
+      const nameString = name ? `in %{name} ` : '';
       return {
         message: `The value entered ${nameString}is not a valid e-mail address`
       };
