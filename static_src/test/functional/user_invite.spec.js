32afe23bda92d54de9725847e837494f0977f4c1
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-06-23 16:43:49 -0400

    Correct for linting

--- static_src/test/functional/user_invite.spec.js
+++ static_src/test/functional/user_invite.spec.js
@@ -67,12 +67,12 @@ describe('User roles', function () {
     });
 
     it('should not have the user invite panel', function () {
+      const ells = browser.elements('.test-users-invite');
       userRoleElement.setAndGetUserRole(cookieManagerOrgXSpaceXX);
       browser.url(urlOrgXSpaceXX);
 
       browser.waitForExist('.test-users');
       browser.waitForExist('.test-users-invite', 500, true);
-      var ells = $$('.test-users-invite');
 
       expect(ells.length).toBe(0);
     });
