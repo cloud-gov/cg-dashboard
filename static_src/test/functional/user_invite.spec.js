e2f8ab251fe948ac112b636a079e5474ec1c17f5
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-06-23 17:00:08 -0400

    Make the check if element doesnt exist

--- static_src/test/functional/user_invite.spec.js
+++ static_src/test/functional/user_invite.spec.js
@@ -67,14 +67,15 @@ describe('User roles', function () {
     });
 
     it('should not have the user invite panel', function () {
-      const ells = browser.elements('.test-users-invite');
+      let ells;
       userRoleElement.setAndGetUserRole(cookieManagerOrgXSpaceXX);
       browser.url(urlOrgXSpaceXX);
 
       browser.waitForExist('.test-users');
       browser.waitForExist('.test-users-invite', 500, true);
+      ells = browser.elements('.test-users-invite');
 
-      expect(ells.length).toBe(0);
+      expect(ells.length).toBe(undefined);
     });
   });
 });
