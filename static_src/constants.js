2c2b8d4cc210e6f5eaa4c94bcb56b6883a2713b0
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-07-25 10:36:13 -0400

    Move the comment to correct action

--- static_src/constants.js
+++ static_src/constants.js
@@ -270,9 +270,9 @@ const userActionTypes = keymirror({
   USER_INVITE_ERROR: null,
   // Action to dismiss an user list notification.
   USER_LIST_NOTICE_DISMISSED: null,
-  // Action when all roles for space user are removed.
+  // Action to remove all roles for space user.
   USER_REMOVE_ALL_SPACE_ROLES: null,
-  // Action to removed all roles for space user.
+  // Action when all roles for space user are removed.
   USER_REMOVED_ALL_SPACE_ROLES: null,
   // Action to delete a user from an org.
   USER_DELETE: null,
