a94ab91a1d2d9773ff3f0ab31aab99f05734c226
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-07-28 17:47:27 -0400

    Get the error working on the component

--- static_src/test/unit/components/users_org_user_selector.spec.jsx
+++ static_src/test/unit/components/users_org_user_selector.spec.jsx
@@ -3,20 +3,37 @@ import '../../global_setup.js';
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Form } from '../../../components/form';
-import ParentUserSelector from '../../../components/users_parent_entity_user_selector.jsx';
+import OrgUsersSelector from '../../../components/users_org_user_selector.jsx';
 
-describe('<ParentUserSelector />', function () {
+describe('<OrgUsersSelector />', function () {
+  const parentEntityType = 'organization';
   const entityType = 'space';
   const props = {
-    inviteEntityType: entityType,
-    currentUserAccess: true
+    currentUserAccess: true,
+    orgUsersSelectorDisabled: false,
+    parentEntityUsers: [],
+    error: {},
+    parentEntity: parentEntityType,
+    currentEntityGuid: 'a-space-guid',
+    currentEntity: entityType
   };
   let wrapper;
+
+  describe('when user sees a error message', () => {
+    it('renders error in form', () => {
+      const error = {};
+      error.contextualMessage = "this is a message";
+      error.message = "this is the medium"
+      const errorProps = Object.assign({}, props, { error });
+      wrapper = shallow(<OrgUsersSelector { ...errorProps } />);
+      expect(wrapper.find(Form).find('.error_message').length).toEqual(1);
+    });
+  });
 
   describe('when user does not have ability to invite other users', () => {
     it('does not render <Form /> component', () => {
       const noAccessProps = Object.assign({}, props, { currentUserAccess: false });
-      wrapper = shallow(<ParentUserSelector { ...noAccessProps } />);
+      wrapper = shallow(<OrgUsersSelector { ...noAccessProps } />);
 
       expect(wrapper.find(Form).length).toEqual(0);
     });
