7a6a9a04e3c609f0714cbf0a7b359491dffb5a71
Adam Biagianti <adam.biagianti@gsa.gov>
2017-08-01 10:02:18 -0400

    Properly extracts the value from form field object

--- static_src/test/unit/components/create_service_instance.spec.jsx
+++ static_src/test/unit/components/create_service_instance.spec.jsx
@@ -6,11 +6,16 @@ import FormError from '../../../components/form/form_error.jsx';
 import Immutable from 'immutable';
 import SpaceStore from '../../../stores/space_store';
 import ServiceInstanceStore from '../../../stores/service_instance_store';
+import serviceActions from '../../../actions/service_actions';
 import { shallow } from 'enzyme';
 
 describe('<CreateServiceInstance />', () => {
   beforeEach(() => {
     ServiceInstanceStore._createError = { description: 'Bad stuff everyone' };
+  });
+
+  afterEach(() => {
+    ServiceInstanceStore._createError = null;
   });
 
   it('displays an error message when ServiceInstanceStore has one', () => {
@@ -19,5 +24,24 @@ describe('<CreateServiceInstance />', () => {
     const wrapper = shallow(<CreateServiceInstance servicePlan={ {} } />);
 
     expect(wrapper.find(FormError).length).toBe(1);
+  });
+
+  describe('._onValidForm()', () => {
+    it('calls serviceActions.createInstance with the proper arguments', () => {
+      const plan = { guid: '123abc' };
+      const spy = sinon.spy(serviceActions, 'createInstance');
+      const wrapper = shallow(<CreateServiceInstance servicePlan={ plan } />);
+      const form = {
+        name: {
+          value: 'test'
+        }
+      };
+
+      wrapper.instance()._onValidForm({}, form);
+
+      expect(spy).toHaveBeenCalledOnce();
+      expect(spy).toHaveBeenCalledWith(form.name.value, null, plan.guid);
+      expect(typeof spy.getCall(0).args[0]).toBe('string');
+    });
   });
 });
