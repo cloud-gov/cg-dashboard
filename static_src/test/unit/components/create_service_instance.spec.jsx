46d775ce648257fea3962de412493a3716fefa3e
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-08-18 14:51:40 -0400

    Create tests for the various create service instance conditions with hardcoded responses

--- static_src/test/unit/components/create_service_instance.spec.jsx
+++ static_src/test/unit/components/create_service_instance.spec.jsx
@@ -7,6 +7,14 @@ import serviceActions from '../../../actions/service_actions';
 import { shallow } from 'enzyme';
 
 describe('<CreateServiceInstance />', () => {
+
+  const serviceProps = {
+    service: {},
+    servicePlan: {
+      guid: 'some-plan-guid'
+    }
+  };
+
   it('displays an error message when ServiceInstanceStore has one', () => {
     const error = { description: 'Bad stuff everyone' };
     const wrapper = shallow(<CreateServiceInstance servicePlan={ {} } error={ error } />);
@@ -30,6 +38,55 @@ describe('<CreateServiceInstance />', () => {
       expect(spy).toHaveBeenCalledOnce();
       expect(spy).toHaveBeenCalledWith(form.name.value, null, plan.guid);
       expect(typeof spy.getCall(0).args[0]).toBe('string');
+    });
+  });
+
+  describe('when serviceAction.createInstance has pre-designated responses', () => {
+    it('called for cdn-route', () => {
+      const serviceInstanceHTML = '<div class="actions-large"><form id="create-service-form"' +
+      ' action="/" method="post" class="test-create_service_instance_form"><fieldset><legend>' +
+      'The<strong class="actions-callout-inline-block">cdn-route</strong> service instance ' +
+      'must be created using the CF CLI. Please refer to <a href="https://cloud.gov/docs/' +
+      'services/cdn-route/" target="_blank">https://cloud.gov/docs/services/cdn-route/</a> ' +
+      'for more information.</legend></fieldset></form></div>';
+      const serviceProps = Object.assign({}, serviceProps, {
+        service: { label: 'cdn-route' }
+      });
+      const wrapper = shallow(<CreateServiceInstance { ...serviceProps } />);
+
+      expect(wrapper.html()).toEqual(serviceInstanceHTML);
+    });
+
+    it('called for cloud-gov-identity-provider', () => {
+      const serviceInstanceHTML = '<div class="actions-large"><form id="create-service-form"' +
+      ' action="/" method="post" class="test-create_service_instance_form"><fieldset><legend>' +
+      'The<strong class="actions-callout-inline-block">cloud-gov-identity-provider</strong> ' +
+      'service instance must be created using the CF CLI. Please refer to <a href="https:' +
+      '//cloud.gov/docs/services/cloud-gov-identity-provider/" target="_blank">https://clo' +
+      'ud.gov/docs/services/cloud-gov-identity-provider/</a> for more information.</legend>' +
+      '</fieldset></form></div>';
+      const serviceProps = Object.assign({}, serviceProps, {
+        service: { label: 'cloud-gov-identity-provider' }
+      });
+      const wrapper = shallow(<CreateServiceInstance { ...serviceProps } />);
+
+      expect(wrapper.html()).toEqual(serviceInstanceHTML);
+    });
+
+    it('called for cloud-gov-service-account', () => {
+      const serviceInstanceHTML = '<div class="actions-large"><form id="create-service-form"' +
+      ' action="/" method="post" class="test-create_service_instance_form"><fieldset><legend>' +
+      'The<strong class="actions-callout-inline-block">cloud-gov-service-account</strong> ' +
+      'service instance must be created using the CF CLI. Please refer to <a href="' +
+      'https://cloud.gov/docs/services/cloud-gov-service-account/" target="_blank">https://' +
+      'cloud.gov/docs/services/cloud-gov-service-account/</a> for more information.</legend>' +
+      '</fieldset></form></div>';
+      const serviceProps = Object.assign({}, serviceProps, {
+        service: { label: 'cloud-gov-service-account' }
+      });
+      const wrapper = shallow(<CreateServiceInstance { ...serviceProps } />);
+
+      expect(wrapper.html()).toEqual(serviceInstanceHTML);
     });
   });
 });
