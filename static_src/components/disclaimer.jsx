82d309f0e3a58e61de34a31ff3066fdc32cd8ca7
Adam Biagianti <adam.biagianti@gsa.gov>
2017-08-31 10:29:59 -0400

    Override css, toggle header content

--- static_src/components/disclaimer.jsx
+++ static_src/components/disclaimer.jsx
@@ -1,5 +1,4 @@
-
-import classNames from 'classnames';
+import classnames from 'classnames';
 import React from 'react';
 import { config } from 'skin';
 
@@ -11,8 +10,34 @@ const dotGovAlt = 'Dot gov';
 const httpsAlt = 'Https';
 const flagAlt = 'US flag signifying that this is a United States Federal Government website';
 
+const CONTROLS = 'gov-banner';
+
 export default class Disclaimer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  }
+
   render() {
+    const { expanded } = this.state;
+    const hidden = !expanded;
+    const panelClass = classnames('usa-grid', {
+      hide: hidden
+    });
+
     let flagImg;
 
     if (config.header.show_flag) {
@@ -20,37 +45,42 @@ export default class Disclaimer extends React.Component {
     }
 
     return (
-      <div className="usa-banner">
-        <div className="usa-accordion">
-          <header className="usa-banner-header">
-            <div className="usa-grid usa-banner-inner">
-                { flagImg }
-                <p>{ config.header.disclaimer }</p>
-                <button className="usa-accordion-button usa-banner-button" aria-expanded='false' aria-controls='gov-banner'>
-                  <span className="usa-banner-button-text">{ config.header.disclaimer_link_text }</span>
-                </button>
+      <div className="usa-banner usa-disclaimer disclaimer-no_sidebar">
+        <header className="grid">
+            <span className='usa-disclaimer-official'>
+              { config.header.disclaimer }
+              { flagImg }
+              <a
+                className="action action-link action-secondary"
+                aria-expanded={ expanded }
+                aria-controls={ CONTROLS }
+                onClick={ this.handleClick }
+              >
+                <span className="p1">{ config.header.disclaimer_link_text }</span>
+                <span className="icon-chevron icon-chevron-down"></span>
+              </a>
+            </span>
+
+        </header>
+        <div className={ panelClass } id={ CONTROLS } aria-hidden={ hidden }>
+          <div className="usa-width-one-half">
+            <img className="usa-banner-icon usa-media_block-img" alt={ dotGovAlt } src={ dotGovIcon } />;
+            <div className="usa-media_block-body">
+              <p>
+                <strong>{ config.header.disclaimer_reason_gov_header }</strong>
+                <br />
+                { config.header.disclaimer_reason_gov_body }
+              </p>
             </div>
-          </header>
-          <div className="usa-banner-content usa-grid usa-accordion-content" id='gov-banner'>
-            <div className="usa-banner-guidance-gov usa-width-one-half">
-              <img className="usa-banner-icon usa-media_block-img" alt={ dotGovAlt } src={ dotGovIcon } />;
-              <div className="usa-media_block-body">
-                <p>
-                  <strong>{ config.header.disclaimer_reason_gov_header }</strong>
-                  <br />
-                  { config.header.disclaimer_reason_gov_body }
-                </p>
-              </div>
-            </div>
-            <div className="usa-banner-guidance-ssl usa-width-one-half">
-              <img className="usa-banner-icon usa-media_block-img" alt={ httpsAlt } src={ httpsIcon } />;
-              <div className="usa-media_block-body">
-                <p>
-                  <strong>{ config.header.disclaimer_reason_https_header }</strong>
-                  <br />
-                  { config.header.disclaimer_reason_https_body }
-                </p>
-              </div>
+          </div>
+          <div className="usa-banner-guidance-ssl usa-width-one-half">
+            <img className="usa-banner-icon usa-media_block-img" alt={ httpsAlt } src={ httpsIcon } />;
+            <div className="usa-media_block-body">
+              <p>
+                <strong>{ config.header.disclaimer_reason_https_header }</strong>
+                <br />
+                { config.header.disclaimer_reason_https_body }
+              </p>
             </div>
           </div>
         </div>
