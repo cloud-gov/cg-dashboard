d47c0cfcc2152fab9fa62241eb61b74ea54e93ba
Jonathan Ingram <jonathan.ingram@digital.gov.au>
2017-11-08 08:03:19 +1100

    Reinstate doc comment for formatDateTime

--- static_src/util/format_date.js
+++ static_src/util/format_date.js
@@ -3,6 +3,15 @@ import moment from 'moment-timezone';
 const makeInvalidDateTimeValueError = str =>
   new Error(`could not format invalid date/time value: ${str}`);
 
+/**
+ * formatDateTime returns a formatted date/time string.
+ *
+ * @params {String} date/time as a string
+ * @params {String} timezone abbreviation - defaults to "Etc/UTC"
+ *
+ * @returns {String} A "MMM DD YYYY hh:mma z" formatted string
+ *                   e.g. "Jul 13 2015 09:02pm PDT"
+*/
 const formatDateTime = (str, timezone = 'Etc/UTC') => {
   if (!str) {
     throw makeInvalidDateTimeValueError(str);
