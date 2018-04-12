1d113549e44ea7acb78556b510207a4dc1558958
Jonathan Carmack <jonathan.carmack@gsa.gov>
2018-04-12 10:30:27 -0400

    Added code initializing cert store and sending using TLS connection

--- mailer/mailer.go
+++ mailer/mailer.go
@@ -1,6 +1,8 @@
 package mailer
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"net/smtp"
 
 	"github.com/18F/cg-dashboard/helpers"
@@ -14,31 +16,49 @@ type Mailer interface {
 
 // InitSMTPMailer creates a new SMTP Mailer
 func InitSMTPMailer(settings helpers.Settings) (Mailer, error) {
+	var tlsConfig *tls.Config
+	if config.Cert != "" {
+		pool := x509.NewCertPool()
+		pool.AppendCertsFromPEM([]byte(settings.SMTPCert))
+		tlsConfig = &tls.Config{
+			ServerName: settings.SMTPHost,
+			RootCAs:    pool,
+		}
+
+	}
 	return &smtpMailer{
-		smtpHost: settings.SMTPHost,
-		smtpPort: settings.SMTPPort,
-		smtpUser: settings.SMTPUser,
-		smtpPass: settings.SMTPPass,
-		smtpFrom: settings.SMTPFrom,
-		smtpCert: settings.SMTPCert,
+		smtpHost:  settings.SMTPHost,
+		smtpPort:  settings.SMTPPort,
+		smtpUser:  settings.SMTPUser,
+		smtpPass:  settings.SMTPPass,
+		smtpFrom:  settings.SMTPFrom,
+		smtpCert:  settings.SMTPCert,
+		tlsConfig: tlsConfig,
 	}, nil
 }
 
 type smtpMailer struct {
-	smtpHost string
-	smtpPort string
-	smtpUser string
-	smtpPass string
-	smtpFrom string
-	smtpCert string
+	smtpHost  string
+	smtpPort  string
+	smtpUser  string
+	smtpPass  string
+	smtpFrom  string
+	smtpCert  string
+	tlsConfig tlsConfig
 }
 
 func (s *smtpMailer) SendEmail(emailAddress, subject string, body []byte) error {
 	e := email.NewEmail()
 	e.From = s.smtpFrom
 	e.To = []string{" <" + emailAddress + ">"}
-
 	e.HTML = body
 	e.Subject = subject
+
+	addr := s.smtpHost + ":" + s.smtpPass
+	auth := smtp.PlainAuth("", s.smtpUser, s.smtpPass, s.smtpHost)
+
+	if s.tlsConfig != nil {
+		return e.SendWithTLS(addr, auth, s.tlsConfig)
+	}
 	return e.Send(s.smtpHost+":"+s.smtpPort, smtp.PlainAuth("", s.smtpUser, s.smtpPass, s.smtpHost))
 }
